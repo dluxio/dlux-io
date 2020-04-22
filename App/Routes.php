620d2b3d7d8f17644297d113f264b90ea47e4fff
Steven Ettinger <sjettingerjr@gmail.com>
2020-04-22 05:42:44 -0500

    four

--- App/Routes.php
+++ App/Routes.php
@@ -7,16 +7,16 @@ $router = new Core\Router();
 
 // Add the routes
 $router->respondWithController('GET', '/@[:name]', 'Me@index');
-$router->respondWithController('GET', '/me', 'Me@index');
+$router->respondWithController('GET', '/me/', 'Me@index');
 //$router->respondWithController('GET', '/me/', 'Me@index');
-$router->respondWithController('GET', '/dex', 'Dex@index');
+$router->respondWithController('GET', '/dex/', 'Dex@index');
 //$router->respondWithController('GET', '/dex/', 'Dex@index');
-$router->respondWithController('GET', '/apps', 'Apps@index');
-$router->respondWithController('GET', '/login', 'Login@index');
-$router->respondWithController('GET', '/blog', 'Blog@index');
-$router->respondWithController('GET', '/dlux', 'Dlux@index');
-$router->respondWithController('GET', '/api', 'Api@index');
-$router->respondWithController('GET', '/dluxar/@[:name]/[:permlink]', 'Dlux@index');
+$router->respondWithController('GET', '/apps/', 'Apps@index');
+$router->respondWithController('GET', '/login/', 'Login@index');
+$router->respondWithController('GET', '/blog/', 'Blog@index');
+$router->respondWithController('GET', '/dlux/', 'Dlux@index');
+$router->respondWithController('GET', '/api/', 'Api@index');
+$router->respondWithController('GET', '/dluxar/@[:name]/[:permlink]/', 'Dlux@index');
 $router->respondWithController('GET', '/', 'Home@index');
 // Dispatch the router
 $router->dispatch();
