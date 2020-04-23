71cf6209a25104853a899c3ed86e3a1bf8502ecb
Steven Ettinger <sjettingerjr@gmail.com>
2020-04-23 10:42:56 -0500

    four

--- App/Views/blog/index.php
+++ App/Views/blog/index.php
@@ -27,8 +27,8 @@
       id="dluxGetReplies"
       is="dmx-fetch"
       url="https://token.dlux.io/getwrap?"
-      dmx-param:method="'condenser_api.get_content_replies'"
-      dmx-param:params="'[%22".$author."%22,%22".$permlink."%22]'"
+      dmx-param:method="\'condenser_api.get_content_replies\'"
+      dmx-param:params="\'[%22".$author."%22,%22".$permlink."%22]\'"
     ></dmx-api-datasource>';?>
     <?php 
    $path = $_SERVER['DOCUMENT_ROOT'];
