ee7d7caf0c626c4127efd5dbc6911d0b3f562171
Steven Ettinger <sjettingerjr@gmail.com>
2020-04-24 17:55:54 -0500

    four

--- public/js/dex.js
+++ public/js/dex.js
@@ -530,11 +530,9 @@ function popOrderTable(orderstable, type) {
     console.log(User.dex.markets[User.opts.pair][type])
     for (i in User.dex.markets[User.opts.pair][type]) {
         let txnode = document.createElement('tr')
-        let whos = `< button class = "btn btn-outline-danger btn-sm"
-    type = "submit" onclick="getSellID('${User.dex.markets[User.opts.pair][type][i].txid}')"> Sell < /button>`
+        let whos = `<button class ="btn btn-outline-danger btn-sm" type="submit" onclick="getSellID('${User.dex.markets[User.opts.pair][type][i].txid}')"> Sell </button>`
         if (User.dex.markets[User.opts.pair][type][i].from == user) {
-            whos = `< button class = "btn btn-outline-warning btn-sm"
-    type = "submit" onclick="cancel('${User.dex.markets[User.opts.pair][type][i].txid}')"> Cancel < /button>`
+            whos = `<button class ="btn btn-outline-warning btn-sm" type="submit" onclick="cancel('${User.dex.markets[User.opts.pair][type][i].txid}')"> Cancel </button>`
         }
         txnode.innerHTML = `
     <td>${parseFloat(User.dex.markets[User.opts.pair][type][i].amount/1000).toFixed(3)}</td> 
