0fd266c223e8234ef113a77f1035d04c4a9eb058
Mark <m.e.giles@gmail.com>
2020-04-26 06:32:23 -0700

    Update dex.js

--- public/js/dex.js
+++ public/js/dex.js
@@ -394,15 +394,11 @@ function dexview(pair, type) {
     popOrderTable('buyorderstable', 'buyOrders')
     popOrderTable('sellorderstable', 'sellOrders')
     if (User.opts.type === 'Buy') {
-        document.getElementById('buyTab').classList.add('active')
-        document.getElementById('sellTab').classList.remove('active')
-        document.getElementById('listButton').innerText = 'Buy'
-        document.getElementById('listButton').classList.toggle('btn-outline-danger btn-outline-success')
+        document.getElementById('listButton').innerText = 'Buy';
+        document.getElementById('listButton').classList.toggle('btn-outline-danger btn-outline-success');
     } else {
-        document.getElementById('buyTab').classList.remove('active')
-        document.getElementById('sellTab').classList.add('active')
-        document.getElementById('listButton').innerText = 'Sell'
-        document.getElementById('listButton').classList.toggle('btn-outline-success btn-outline-danger')
+        document.getElementById('listButton').innerText = 'Sell';
+        document.getElementById('listButton').classList.toggle('btn-outline-success btn-outline-danger');
     }
     if (User.opts.pair === 'hive') {
         document.getElementById('pairmenustatus').innerText = 'DLUX:HIVE'
