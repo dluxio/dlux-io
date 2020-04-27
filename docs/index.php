1d7d7b56af2a9cf9068f4d433f8f739d679a94d5
Mark <m.e.giles@gmail.com>
2020-04-27 11:12:42 -0700

    Update index.php

--- docs/index.php
+++ docs/index.php
@@ -1,1 +1,22 @@
-Hello World
+<!doctype html>
+<html lang="en" class="h-100">
+<head>
+<title>DLUX - Apps</title>
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/mod/header.php";
+   include_once($path);
+?>
+<body class="d-flex flex-column h-100">
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/mod/nav.php";
+   include_once($path);
+?>
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/mod/footer.php";
+   include_once($path);
+?>
+</body>
+</html>
