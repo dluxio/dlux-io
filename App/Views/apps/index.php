71dd33db200619b27217995f66314869e9dbdeb5
Mark <m.e.giles@gmail.com>
2020-12-10 12:48:06 -0800

    fixed it

--- App/Views/apps/index.php
+++ App/Views/apps/index.php
@@ -15,7 +15,7 @@
 </head>
 
 <body id="index" is="dmx-app" class="d-flex flex-column h-100">
-<dmx-api-datasource id="dluxGetBlog" is="dmx-fetch" url="https://token.dlux.io/getwrap?" dmx-param:method="'tags_api.get_discussions_by_blog'" dmx-param:params="'[%22robotolux%22]'"></dmx-api-datasource>
+<dmx-api-datasource id="dluxGetBlog" is="dmx-fetch" url="https://token.dlux.io/getwrap?" dmx-param:method="'tags_api.get_discussions_by_blog'" dmx-param:params="'[%22robotolux%22,0,10]'"></dmx-api-datasource>
 <?php 
    $path = $_SERVER['DOCUMENT_ROOT'];
    $path .= "/mod/nav.php";
@@ -36,21 +36,21 @@
   <div class="card text-white bg-dark mt-2 mb-3">
     <div class="card-header">
       <div class="d-inline-block">
-        <div class="float-left" ><a dmx-bind:href="/@{{comment.author}}"><img dmx-bind:src="https://token.dlux.io/getauthorpic/{{comment.author}}" alt="" class="rounded-circle bg-light img-fluid mr-2 cover author-img"></a></div>
+        <div class="float-left" ><a dmx-bind:href="/@{{author}}"><img dmx-bind:src="https://token.dlux.io/getauthorpic/{{author}}" alt="" class="rounded-circle bg-light img-fluid mr-2 cover author-img"></a></div>
         <div class="float-left">
-          <p class="mt-0 mb-0 text-muted text-semibold"><a dmx-bind:href="/@{{comment.author}}" class="a-1">{{comment.author}}<span class="ml-2 badge badge-pill badge-light">{{comment.author_reputation.toString().rep()}}</span></a></p>
-          <small class="text-muted">{{comment.created.formatDate("MMM dd, yyyy")}}</small></div>
+          <p class="mt-0 mb-0 text-muted text-semibold"><a dmx-bind:href="/@{{author}}" class="a-1">{{author}}<span class="ml-2 badge badge-pill badge-light">{{author_reputation.toString().rep()}}</span></a></p>
+          <small class="text-muted">{{formatDate("MMM dd, yyyy")}}</small></div>
       </div>
-      <div class="float-right"><span class="badge badge-secondary">{{comment.json_metadata.scat()}}</span></div>
+      <div class="float-right"><span class="badge badge-secondary">{{json_metadata.scat()}}</span></div>
     </div>
-    <a href="#detailModal" class="a-1" data-toggle="modal" dmx-on:click="app_detail.select(entry_id)" dmx-bind:onclick="window.history.pushState('{{comment.url}}','{{comment.title}}', '/blog/@{{comment.author}}/{{comment.permlink}}');">
-      <h5 class="card-title mt-2 text-center text-capitalize">{{comment.title}}</h5>
-      <img src="..."  alt="Card image cap" class="card-img-top" dmx-bind:src="{{comment.json_metadata.parseJSON().image}}" /></a>
+    <a href="#detailModal" class="a-1" data-toggle="modal" dmx-on:click="app_detail.select(entry_id)" dmx-bind:onclick="window.history.pushState('{{url}}','{{title}}', '/blog/@{{author}}/{{permlink}}');">
+      <h5 class="card-title mt-2 text-center text-capitalize">{{title}}</h5>
+      <img src="..."  alt="Card image cap" class="card-img-top" dmx-bind:src="{{json_metadata.parseJSON().image}}" /></a>
     <div class="card-body"><a href="#detailModal" data-toggle="modal" class="a-1">
-      <p class="preview-text">{{comment.body.removeMD().trunc(100,true,"...")}}</p>
+      <p class="preview-text">{{body.removeMD().trunc(100,true,"...")}}</p>
     </a></div>
     <center>
-      <a dmx-bind:href="{{comment.url}}" type="button" class="btn btn-outline-danger mb-4 btn-launch">Launch App</a>
+      <a dmx-bind:href="{{url}}" type="button" class="btn btn-outline-danger mb-4 btn-launch">Launch App</a>
     </center>
     <div class="card-footer">
 		<div class="collapse" dmx-bind:id="vote{{entry_id}}">
