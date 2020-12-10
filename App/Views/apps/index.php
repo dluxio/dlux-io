b4d05129c32ea98ddab13a6ff88394bb694ce902
Mark <m.e.giles@gmail.com>
2020-12-10 14:24:05 -0800

    fixed footer

--- App/Views/apps/index.php
+++ App/Views/apps/index.php
@@ -53,12 +53,12 @@
       <a dmx-bind:href="{{url}}" type="button" class="btn btn-outline-danger mb-4 btn-launch">Launch App</a>
     </center>
     <div class="card-footer">
-		<div class="collapse" dmx-bind:id="vote{{entry_id}}">
+		<div class="collapse" dmx-bind:id="vote{{data.entry_id}}">
 		<form id="voteForm">
   		<div class="form-group">
 
     	<ul class="list-unstyled">
-			<li class="float-left px-1"><button type="button" class="btn btn-primary" dmx-bind:id="voteBtn{{entry_id}}" dmx-bind:onclick="vote('{{comment.author}}','{{comment.permlink}}','slider{{entry_id}}')" style="width:70px">100%</button></li>
+			<li class="float-left px-1"><button type="button" class="btn btn-primary" dmx-bind:id="voteBtn{{entry_id}}" dmx-bind:onclick="vote('{{author}}','{{permlink}}','slider{{entry_id}}')" style="width:70px">100%</button></li>
 			<li class="float-left px-1"><button type="button" class="btn btn-secondary" data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{entry_id}}"><span class="close text-white">×</span></button></li>
 		</ul>
 		<ul class="float-right list-unstyled">
@@ -71,9 +71,9 @@
 </form>
 			</div>
       <div class="d-inline-block">
-        <div class="float-left"><a data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{entry_id}}"><i class="fas fa-heart mr-1"></i></a>{{comment.active_votes.countUpVotes()}} <i class="fas fa-comment ml-2 mr-1"></i>{{comment.children}}</div>
+        <div class="float-left"><a data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{entry_id}}"><i class="fas fa-heart mr-1"></i></a>{{active_votes.countUpVotes()}} <i class="fas fa-comment ml-2 mr-1"></i>{{children}}</div>
       </div>
-      <div class="float-right">{{comment.total_payout_value}} <img src="/img/hextacular.svg" alt="" width="17"/></div>
+      <div class="float-right">{{total_payout_value}} <img src="/img/hextacular.svg" alt="" width="17"/></div>
     </div>
   </div>
 </div>
@@ -82,20 +82,20 @@
    <div class="modal-content bg-dark text-white">
 	<dmx-data-detail id="app_detail" dmx-bind:data="dluxGetBlog.data.result" key="entry_id">
 		<div class="d-inline-block p-2">
-	  <div class="float-left" ><a dmx-bind:href="/@{{comment.author}}"><img dmx-bind:src="https://token.dlux.io/getauthorpic/{{data.comment.author}}" alt="" class="rounded-circle bg-light img-fluid mr-2 cover author-img"></a></div>
+	  <div class="float-left" ><a dmx-bind:href="/@{{author}}"><img dmx-bind:src="https://token.dlux.io/getauthorpic/{{data.author}}" alt="" class="rounded-circle bg-light img-fluid mr-2 cover author-img"></a></div>
         <div class="float-left">
-          <p class="mt-0 mb-0 text-muted text-semibold"><a dmx-bind:href="/@{{comment.author}}" class="a-1">{{data.comment.author}}<span class="ml-2 badge badge-pill badge-light">{{data.comment.author_reputation.toString().rep()}}</span></a></p>
-          <small class="text-muted">{{data.comment.created.formatDate("MMM dd, yyyy")}}</small></div>
+          <p class="mt-0 mb-0 text-muted text-semibold"><a dmx-bind:href="/@{{author}}" class="a-1">{{data.author}}<span class="ml-2 badge badge-pill badge-light">{{data.author_reputation.toString().rep()}}</span></a></p>
+          <small class="text-muted">{{data.created.formatDate("MMM dd, yyyy")}}</small></div>
 		</div>
-      <div class="float-right p-2"><span class="badge badge-secondary">{{data.comment.json_metadata.scat()}}</span><button type="button" class="close text-white ml-3" data-dismiss="modal" aria-label="Close" onclick="window.history.back();"><span aria-hidden="true">×</span></button></div>
+      <div class="float-right p-2"><span class="badge badge-secondary">{{data.json_metadata.scat()}}</span><button type="button" class="close text-white ml-3" data-dismiss="modal" aria-label="Close" onclick="window.history.back();"><span aria-hidden="true">×</span></button></div>
 		<hr class="mt-0">
-    <a dmx-bind:href="/blog/@{{data.comment.author}}/{{data.comment.permlink}}"><h4 class="text-center p-2">{{data.comment.title}}</h4></a>
+    <a dmx-bind:href="/blog/@{{data.author}}/{{data.permlink}}"><h4 class="text-center p-2">{{data.title}}</h4></a>
     
 
-	 <img src="..."  alt="Card image cap" class="card-img-top" dmx-bind:src="{{data.comment.json_metadata.parseJSON().image}}" />  
-    <p class="p-2">{{data.comment.body.removeMD()}}</p>
+	 <img src="..."  alt="Card image cap" class="card-img-top" dmx-bind:src="{{data.json_metadata.parseJSON().image}}" />  
+    <p class="p-2">{{data.body.removeMD()}}</p>
 		  <center>
-      <a dmx-bind:href="{{data.comment.url}}" type="button" class="btn btn-outline-danger mb-4 btn-launch">Launch App</a>
+      <a dmx-bind:href="{{data.url}}" type="button" class="btn btn-outline-danger mb-4 btn-launch">Launch App</a>
     </center>
 <hr class="mb-0">
 		<div class="collapse" dmx-bind:id="vote{{entry_id}}">
@@ -103,7 +103,7 @@
   		<div class="form-group">
 
     	<ul class="list-unstyled">
-			<li class="float-left px-1"><button type="button" class="btn btn-primary" dmx-bind:id="voteBtn{{entry_id}}" dmx-bind:onclick="vote('{{comment.author}}','{{comment.permlink}}','slider{{entry_id}}')" style="width:70px">100%</button></li>
+			<li class="float-left px-1"><button type="button" class="btn btn-primary" dmx-bind:id="voteBtn{{entry_id}}" dmx-bind:onclick="vote('{{author}}','{{permlink}}','slider{{entry_id}}')" style="width:70px">100%</button></li>
 			<li class="float-left px-1"><button type="button" class="btn btn-secondary" data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{entry_id}}"><span class="close text-white">×</span></button></li>
 		</ul>
 		<ul class="float-right list-unstyled">
@@ -116,9 +116,9 @@
 </form>
 			</div>
 		<div class="d-inline-block p-2">
-        <a data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{entry_id}}"><i class="fas fa-heart mr-1"></i></a>{{data.comment.active_votes.countUpVotes()}} <i class="fas fa-comment ml-2 mr-1"></i>{{data.comment.children}}</div>
+        <a data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{entry_id}}"><i class="fas fa-heart mr-1"></i></a>{{data.active_votes.countUpVotes()}} <i class="fas fa-comment ml-2 mr-1"></i>{{data.children}}</div>
       
-      <div class="float-right p-2">{{data.comment.total_payout_value}} <img src="../img/hextacular.svg" alt="" width="17"/></div>
+      <div class="float-right p-2">{{data.total_payout_value}} <img src="../img/hextacular.svg" alt="" width="17"/></div>
 </div>
   </div>
 </dmx-data-detail>
