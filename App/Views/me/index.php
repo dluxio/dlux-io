68915828ed81694d1e4566f0b3cad2c81cdc2929
Mark <m.e.giles@gmail.com>
2021-01-05 20:19:59 -0800

    Update index.php

--- App/Views/me/index.php
+++ App/Views/me/index.php
@@ -18,8 +18,6 @@
 <script type="text/javascript" src="/dmxAppConnect/dmxFormatter/dmxFormatter.js"></script>
 </head>
 <body class="d-flex flex-column bg-darker h-100" id="apps" is="dmx-app">
-	
-<dmx-api-datasource id="dluxGetBlog" is="dmx-fetch" url="https://token.dlux.io/api/condenser_api/get_discussions_by_blog/" dmx-param:tag="'robotolux'" dmx-param:limit="15"></dmx-api-datasource>
 <?php 
 	if(isset($author)){
     	echo "<dmx-api-datasource id=\"dluxGetBlog\" is=\"dmx-fetch\" url=\"https://token.dlux.io/api/condenser_api/get_discussions_by_blog\" 
