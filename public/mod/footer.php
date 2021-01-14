2ea8c028731f283659489adde730b4b5b2dd098a
Steven Ettinger <sjettingerjr@gmail.com>
2021-01-14 16:37:45 -0300

    shinny things

--- public/mod/footer.php
+++ public/mod/footer.php
@@ -38,24 +38,20 @@
 <!-- Footer -->
 <script>
 function hiveKeychain () {
-    console.log('clicked')
     let username = document.getElementById('hk-username').value
     console.log('value: ' + username)
 	sessionStorage.setItem("user", username);
     let session = new Dluxsession(hive, {hiveidip:username})
 } 
-hive.api.login('', '', function(err, result) {
-  console.log(err, result);
-  let use = document.getElementById("hiveKeychain");
+   window.addEventListener('load', function () {
+    let use = document.getElementById("hiveKeychain");
     let get = document.getElementById("getKeychain");
     if(window.hive_keychain) {
         get.style.display = "none";
     } else {
         use.style.display = "none";
-    }
-checkCookie()
-});
-    
-setTimeout(function(){
-}, 1000);
+    } 
+  checkCookie()
+}) 
+
 </script>
