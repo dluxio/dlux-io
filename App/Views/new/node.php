4d4f07036d7144fb9a856758e90a28179dd74efe
Mark <m.e.giles@gmail.com>
2021-01-19 10:59:33 -0800

    Update node.php

--- App/Views/new/node.php
+++ App/Views/new/node.php
@@ -15,7 +15,58 @@
    $path .= "/mod/nav.php";
    include_once($path);
 ?>
-<p class="text-white">TEST</p>
+<main role="main" class="flex-shrink-0">
+  <div class="container">
+	  <h1 class="text-center text-white">Run A DLUX Node</h1>
+	  <div class="card-deck">
+	<div class="card border-danger bg-dark my-5">
+      <div class="card-body text-center text-white">
+    	<h5 class="card-title">BYOS</h5>
+    	<p class="card-text">Bring Your Own Server</p>
+		<hr class="bg-light">
+		<center>
+		<ul class="text-left">
+		<li>Node.js</li>
+		<li>npm</li>
+		<li>Price: varies</li>
+		</ul>
+		</center>
+    	<a href="#" class="btn btn-danger">Get Started</a>
+  	  </div>
+	</div>
+	<div class="card border-danger bg-dark my-5">
+      <div class="card-body text-center text-white">
+    	<h5 class="card-title">CLOUD</h5>
+    	<p class="card-text">Use a cloud service</p>
+		<hr class="bg-light">
+		<center>
+		<ul class="text-left">
+		<li>Heroku</li>
+		<li>AWS</li>
+		<li>Price: $0-10</li>
+		</ul>
+		</center>
+    	<a href="#" class="btn btn-danger">Get Started</a>
+  	  </div>
+	</div>
+	<div class="card border-danger bg-dark my-5">
+      <div class="card-body text-center text-white">
+    	<h5 class="card-title">RISHI SERVER</h5>
+    	<p class="card-text">Bare metal with easy install</p>
+		<hr class="bg-light">
+		<center>
+		<ul class="text-left">
+		<li>SSH</li>
+		<li>Auto install</li>
+		<li>Price: $5</li>
+		</ul>
+		</center>
+    	<a href="#" class="btn btn-danger">Get Started</a>
+  	  </div>
+	</div>	
+	</div>
+	  </div>
+	</main>
 <?php 
    $path = $_SERVER['DOCUMENT_ROOT'];
    $path .= "/mod/footer.php";
