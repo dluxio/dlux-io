419e11a46502fc782f11857cdfbab1d331e0a85e
Steven Ettinger <sjettingerjr@gmail.com>
2021-01-27 21:05:06 -0300

    scripting

--- App/Views/new/360-gallery.php
+++ App/Views/new/360-gallery.php
@@ -71,7 +71,6 @@
 		</div>
       </div>
 		<div id="listOfImgs" class="flex-column flex-shrink mx-3">
-		<!--
 			<div id="item01" class="p-3 mb-3 bg-dark" style="border-radius: 10px;">
 				<div class="d-flex align-items-center flex-row pb-2 mb-2" style="border-bottom-style: solid; border-bottom-color: #909090">
 					<form class="form-inline"><input class="form-control form-control-sm mr-2" type="text" placeholder="Item name" disabled><button class="btn btn-secondary btn-sm ml-auto" data-toggle="tooltip" data-placement="top" title="Edit Name"><i class="fas fa-fw fa-pencil-alt"></i></button></form>
@@ -87,7 +86,6 @@
 					<div class="small"><a href="#" id="tHashImg01" target="_blank">Thumbnail</a></div>
 		  		</div>
 			</div>
-			-->
 		</div>
 		</div>
 	  <div class="flex-column flex-fill">
@@ -257,7 +255,7 @@ for(var i = 0; i < custom_json.assets.length; i++){
 		item.setAttribute('style', 'border-radius', '10px')
 		item.innerHTML = `<div class="d-flex align-items-center flex-row pb-2 mb-2" style="border-bottom-style: solid; border-bottom-color: #909090">
 					<form class="form-inline"><input id="nameOf${img.hash}" class="form-control form-control-sm mr-2" type="text" placeholder="${img.name}" disabled><button onclick="toggleNameEdit('${img.hash}')" class="btn btn-secondary btn-sm ml-auto" data-toggle="tooltip" data-placement="top" title="Edit Name"><i class="fas fa-fw fa-pencil-alt"></i></button></form>
-					<div class="ml-auto"><button onclick="togglePin('${img.hash}')" class="btn ${btnclass} btn-sm mr-2" data-toggle="tooltip" data-placement="top" title="Asset Pinned to IPFS"><i class="fas fa-fw fa-thumbtack"></i></button><button class="btn btn-danger btn-sm" onclick="deleteImg('${img.hash}')" data-toggle="tooltip" data-placement="top" title="Delete Asset"><i class="fas fa-fw fa-trash-alt"></i></button></div>
+					<div class="ml-auto"><button type="button" onclick="togglePin('${img.hash}')" class="btn ${btnclass} btn-sm mr-2" data-toggle="tooltip" data-placement="top" title="Asset Pinned to IPFS"><i class="fas fa-fw fa-thumbtack"></i></button><button class="btn btn-danger btn-sm" onclick="deleteImg('${img.hash}')" data-toggle="tooltip" data-placement="top" title="Delete Asset"><i class="fas fa-fw fa-trash-alt"></i></button></div>
 				</div>
 				<div class="d-flex flex-row">
 		  		<div id="source${i}" class="mr-3">
