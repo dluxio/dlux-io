ea37b4b6d283add80aa79c4faf73340d89803b7d
Steven Ettinger <sjettingerjr@gmail.com>
2021-02-05 04:05:47 -0300

    crying

--- App/Views/new/360-tour.php
+++ App/Views/new/360-tour.php
@@ -377,8 +377,7 @@ for(var i = 0; i < custom_json.assets.length; i++){
 		window[`image${i}Src`].onload = (function (i){return function() {
 			const img = document.getElementById(`image${i}Src`)
 			console.log({i}, img, window[`image${i}SVG`])
-			const node = document.getElementById(`image${i}SVG`)
-      		node.setAttribute("viewBox", "0 0 " + img.width + " " + img.height);
+      		window[`image${i}SVG`].node.setAttribute("viewBox", "0 0 " + img.width + " " + img.height);
 		}})(i);
 		window[`image${i}SVG`].on(
   "mousedown",
