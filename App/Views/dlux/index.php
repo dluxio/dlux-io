47cac943b87d05da66f3ffac9075a473002d78c4
Steven Ettinger <sjettingerjr@gmail.com>
2021-02-17 02:58:16 -0300

    crying

--- App/Views/dlux/index.php
+++ App/Views/dlux/index.php
@@ -48,11 +48,7 @@
             if (hashy.split('/')[3] == 'ipfs') {
                 hashy = hashy.split('/')[4];
             }
-            var subauthor = author
-            subauthor.replace('.', '-')
-            if(subauthor.find('.')[0]){
-                console.alert(subauthor)
-            }
+            const subauthor = author.replace('.', '-')
             ipfsdomain = `https://${subauthor}.ipfs.dlux.io`;
             location.href = ipfsdomain + `${vars}&hash=${hashy}&author=${author}&permlink=${permlink}&user=${sessionStorage.getItem('user')}`
             
