559f6a39a76e943058d8c61ad6a9468b7818d078
Steven Ettinger <sjettingerjr@gmail.com>
2021-02-17 19:51:14 -0300

    oopsie

--- App/Views/dlux/index.php
+++ App/Views/dlux/index.php
@@ -28,7 +28,7 @@
             echo "const author = '".$author."',";
             echo " permlink = '".$permlink."',";
             ;?>
-         const hiveKey = `${author}/${permlink}`
+        hiveKey = `${author}/${permlink}`
         //hive.api.setOptions({ url: 'https://anyx.io'})
         hive.api.getContent(author, permlink, (err, result) => {
             if(err)console.log(err)
