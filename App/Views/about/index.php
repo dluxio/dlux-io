74635832b6c631347851cb0769b878f561b26070
Mark <m.e.giles@gmail.com>
2021-02-26 22:31:38 -0800

    Update index.php

--- App/Views/about/index.php
+++ App/Views/about/index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en" class="h-100">
 <head>
-<title>DLUX - Me</title>
+<title>DLUX - About</title>
 <?php 
    $path = $_SERVER['DOCUMENT_ROOT'];
    $path .= "/mod/header.php";
