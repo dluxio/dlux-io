0963e1be959123ed348cdce60e958ea4345fbc06
Mark <m.e.giles@gmail.com>
2021-06-25 18:00:56 -0700

    Update index.php

--- App/Views/apps/index.php
+++ App/Views/apps/index.php
@@ -15,7 +15,9 @@
 </head>
 
 <body class="d-flex flex-column h-100 padme-t70" id="index" is="dmx-app">
-<dmx-api-datasource id="dluxGetBlog" is="dmx-fetch" url="https://dluxdata.herokuapp.com/api/condenser_api/get_discussions_by_blog?tag=robotolux"></dmx-api-datasource> // new | hot | trending
+<dmx-api-datasource id="dluxList" is="dmx-fetch" url="https://dluxdata.herokuapp.com/new"></dmx-api-datasource>
+<!-- <dmx-api-datasource id="dluxGetContent" is="dmx-fetch" url="https://token.dlux.io/api/condenser_api/get_content?author=dlux-io&permlink=testing-dlux-vr"></dmx-api-datasource> -->
+// new | hot | trending
 <?php 
    $path = $_SERVER['DOCUMENT_ROOT'];
    $path .= "/mod/nav.php";
@@ -32,7 +34,8 @@
       <div class="col-md-4 text-center m-auto"> <a class="btn btn-primary btn-lg m-1 btn-1" href="/docs" role="button">Learn<i class="fas fa-shapes ml-2"></i></a><a class="btn btn-primary btn-lg m-1 btn-1" href="/new" role="button">Create<i class="fas fa-plus-circle ml-2"></i></a></div>
     </div>
   </div>
-  <div class="card-columns p-3" id="blogResult" is="dmx-repeat" dmx-bind:repeat="dluxGetBlog.data.result">
+  <div class="card-columns p-3" id="blogResult" is="dmx-repeat" dmx-bind:repeat="dluxList.data.result">
+	  <dmx-api-datasource id="dluxLoad" is="dmx-fetch" dmx-bind:url="https://dluxdata.herokuapp.com/api/condenser_api/get_content?author={{author}}&permlink={{permlink}}"></dmx-api-datasource>
     <div class="card text-white bg-dark mt-2 mb-3">
       <div class="card-header">
         <div class="d-inline-block">
@@ -41,29 +44,29 @@
             <p class="mt-0 mb-0 text-muted text-semibold"><a dmx-bind:href="/@{{author}}" class="a-1">{{author}}<span class="ml-2 badge badge-pill badge-light">{{author_reputation.toString().rep()}}</span></a></p>
             <small class="text-muted">{{created.formatDate("MMM dd, yyyy")}}</small></div>
         </div>
-        <div class="float-right"><span class="badge badge-secondary">{{json_metadata.scat()}}</span></div>
+        <div class="float-right"><span class="badge badge-secondary">{{dluxLoad.data.result.json_metadata.scat()}}</span></div>
       </div>
       <a href="#detailModal" class="a-1" data-toggle="modal" dmx-on:click="data_detail.select(url)" dmx-bind:onclick="window.history.pushState('{{url}}','{{title}}', '/blog/@{{author}}/{{permlink}}');">
-        <h5 class="card-title mt-2 text-center text-capitalize">{{title}}</h5>
-        <img src="..."  alt="Card image cap" class="card-img-top" dmx-bind:src="{{json_metadata.parseJSON().picFind()}}" />
+        <h5 class="card-title mt-2 text-center text-capitalize">{{dluxLoad.data.result.title}}</h5>
+        <img src="..."  alt="Card image cap" class="card-img-top" dmx-bind:src="{{dluxLoad.data.result.json_metadata.parseJSON().picFind()}}" />
         <div class="card-body">
-          <p class="preview-text">{{body.removeMD().trunc(100,true,"...")}}</p>
+          <p class="preview-text">{{dluxLoad.data.result.body.removeMD().trunc(100,true,"...")}}</p>
         </div>
       </a>
       <center>
         <a dmx-bind:href="{{url}}" type="button" class="btn btn-outline-danger mb-4 btn-launch" target="_blank">Launch App</a>
       </center>
       <div class="card-footer">
-               <div class="collapse" dmx-bind:id="vote{{post_id}}">
+               <div class="collapse" dmx-bind:id="vote{{dluxLoad.data.result.post_id}}">
                  <form id="voteForm">
 					<div class="d-flex align-items-center text-white-50">
  					  <div>
-                         <button type="button" class="btn btn-primary" dmx-bind:id="voteBtn{{post_id}}" dmx-bind:onclick="vote('{{author}}','{{permlink}}','slider{{post_id}}')">100%</button>
-                         <button type="button" class="btn btn-secondary" data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{post_id}}"><span class="close text-white">×</span></button>
+                         <button type="button" class="btn btn-primary" dmx-bind:id="voteBtn{{dluxLoad.data.result.post_id}}" dmx-bind:onclick="vote('{{dluxLoad.data.result.author}}','{{dluxLoad.data.result.permlink}}','slider{{dluxLoad.data.result.post_id}}')">100%</button>
+                         <button type="button" class="btn btn-secondary" data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{dluxLoad.data.result.post_id}}"><span class="close text-white">×</span></button>
 					  </div>
 				      <div class="flex-fill mx-2">
 			            <p class="my-1">
-							<input type="range" class="form-control-range" step="0.01" value="100" dmx-bind:id="slider{{post_id}}" dmx-bind:onchange="updateVoteSubmit('voteBtn{{post_id}}','slider{{post_id}}');">
+							<input type="range" class="form-control-range" step="0.01" value="100" dmx-bind:id="slider{{dluxLoad.data.result.post_id}}" dmx-bind:onchange="updateVoteSubmit('voteBtn{{dluxLoad.data.result.post_id}}','slider{{dluxLoad.data.result.post_id}}');">
 						  </p>
 					</div>
 						<div>
@@ -73,13 +76,13 @@
                  </form>
                </div> 
                <div class="d-flex align-items-center my-2">
-                 <div><a data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{post_id}}"><i class="fas fa-heart fa-fw mr-1"></i></a>{{active_votes.countUpVotes()}} <i class="fas fa-comment fa-fw ml-2 mr-1"></i>{{children}}</div>
-                 <div class="ml-auto">{{total_payout_value}}<i class="ml-1 fab fa-fw fa-hive"></i></div>
+                 <div><a data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{dluxLoad.data.result.post_id}}"><i class="fas fa-heart fa-fw mr-1"></i></a>{{dluxLoad.data.result.active_votes.countUpVotes()}} <i class="fas fa-comment fa-fw ml-2 mr-1"></i>{{dluxLoad.data.result.children}}</div>
+                 <div class="ml-auto">{{dluxLoad.data.result.total_payout_value}}<i class="ml-1 fab fa-fw fa-hive"></i></div>
                </div>
            </div>
     </div>
   </div>
-  <dmx-data-detail id="data_detail" dmx-bind:data="dluxGetBlog.data.result" key="url">
+  <dmx-data-detail id="data_detail" dmx-bind:data="dluxList.data.result" key="url">
   <div class="modal fade" id="detailModal" tabindex="11" role="dialog" aria-hidden="true">
  <div class="modal-dialog modal-full modal-xl modal-dialog-centered" role="document">
    <div class="modal-content bg-dark text-white">
