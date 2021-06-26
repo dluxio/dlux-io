64f493ebb0d295f43e1faf57d08d6ca6312f91b2
Mark <m.e.giles@gmail.com>
2021-06-25 18:43:25 -0700

    Update index.php

--- App/Views/apps/index.php
+++ App/Views/apps/index.php
@@ -83,56 +83,59 @@
     </div>
   </div>
   <dmx-data-detail id="data_detail" dmx-bind:data="dluxList.data.result" key="url">
-  <div class="modal fade" id="detailModal" tabindex="11" role="dialog" aria-hidden="true">
- <div class="modal-dialog modal-full modal-xl modal-dialog-centered" role="document">
-   <div class="modal-content bg-dark text-white">
-	   <div class="card text-white bg-dark">
-		   <div class="card-header">
-	   		<div class="d-inline-block p-2">
-	  <div class="float-left" ><a dmx-bind:href="/@{{data_detail.data.author}}"><img dmx-bind:src="https://images.hive.blog/u/{{data_detail.data.author}}/avatar" alt="" class="rounded-circle bg-light img-fluid mr-2 cover author-img"></a></div>
-        <div class="float-left">
-          <p class="mt-0 mb-0 text-muted text-semibold"><a dmx-bind:href="/@{{data_detail.data.author}}" class="a-1">{{data_detail.data.author}}<span class="ml-2 badge badge-pill badge-light">{{data_detail.data.author_reputation.toString().rep()}}</span></a></p>
-          <small class="text-muted">{{data_detail.data.created.formatDate("MMM dd, yyyy")}}</small></div>
-		</div>
-      <div class="float-right p-2"><span class="badge badge-secondary">{{data_detail.data.json_metadata.scat()}}</span><button type="button" class="close text-white ml-3" data-dismiss="modal" aria-label="Close" onclick="window.history.back();"><span aria-hidden="true">×</span></button></div>
-		   </div>
-    <a dmx-bind:href="/blog/@{{data_detail.data.author}}/{{data_detail.data.permlink}}" class="text-white"><h3 class="card-title mt-2 text-center text-capitalize p-2">{{data_detail.data.title}}</h3></a>
-	 <img src="..."  alt="Card image cap" class="card-img-top" dmx-bind:src="{{data_detail.data.json_metadata.parseJSON().picFind()}}" />  
-    <div class="card-body">
-		   <p class="p-2">{{data_detail.data.body.removeMD()}}</p>
-		   </div>
-		  <center>
-      <a dmx-bind:href="{{data_detail.data.url}}" type="button" class="btn btn-outline-danger mb-4 btn-launch">Launch App</a>
-    </center>
-	<div class="card-footer">
-		<div class="collapse" dmx-bind:id="vote{{post_id}}">
-		<form id="voteForm">
-  		<div class="d-flex align-items-center text-white-50">
-          <div>
-			<button type="button" class="btn btn-primary" dmx-bind:id="voteBtn{{post_id}}" dmx-bind:onclick="vote('{{comment.author}}','{{data_detail.data.comment.permlink}}','slider{{post_id}}')">100%</button>
-			<button type="button" class="btn btn-secondary" data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{post_id}}"><span class="close text-white">×</span></button>
-		  </div>
-		  <div class="flex-fill mx-2">
-            <p class="my-1">
-			<input type="range" class="form-control-range" step="0.01" value="100" dmx-bind:id="slider{{post_id}}" dmx-bind:onchange="updateVoteSubmit('voteBtn{{post_id}}','slider{{post_id}}');">
-			</p>
-		  </div>
-			<p class="my-0"><span class="mr-1"  id="commentVal">0</span><i class="ml-1 fab fa-fw fa-hive"></i></p>
- 		 </div>
-	  </form>
-	 </div>
-		<div class="d-flex align-items-center my-2">
-		<div><a data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{post_id}}"><i class="fas fa-heart mr-1"></i></a>{{data_detail.data.active_votes.countUpVotes()}} <i class="fas fa-comment ml-2 mr-1"></i>{{data_detail.data.children}}</div>
-     	<div class="ml-auto"> {{data_detail.data.total_payout_value}}<i class="ml-1 fab fa-fw fa-hive"></i></div>
-		</div>
-	   </div>
-	   </div>
-
-		   </div>
-		   </div>
-	 </div>
-</dmx-data-detail>
-	 </div>
+    <div class="modal fade" id="detailModal" tabindex="11" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-full modal-xl modal-dialog-centered" role="document">
+        <div class="modal-content bg-dark text-white">
+          <div class="card text-white bg-dark">
+            <div class="card-header">
+              <div class="d-inline-block p-2">
+                <div class="float-left" ><a dmx-bind:href="/@{{data_detail.data.author}}"><img dmx-bind:src="https://images.hive.blog/u/{{data_detail.data.author}}/avatar" alt="" class="rounded-circle bg-light img-fluid mr-2 cover author-img"></a></div>
+                <div class="float-left">
+                  <p class="mt-0 mb-0 text-muted text-semibold"><a dmx-bind:href="/@{{data_detail.data.author}}" class="a-1">{{data_detail.data.author}}<span class="ml-2 badge badge-pill badge-light">{{data_detail.data.author_reputation.toString().rep()}}</span></a></p>
+                  <small class="text-muted">{{data_detail.data.created.formatDate("MMM dd, yyyy")}}</small></div>
+              </div>
+              <div class="float-right p-2"><span class="badge badge-secondary"> {{blogResult[0].dluxLoad.data.json_metadata.scat()}}</span>
+                <button type="button" class="close text-white ml-3" data-dismiss="modal" aria-label="Close" onclick="window.history.back();"><span aria-hidden="true">×</span></button>
+              </div>
+            </div>
+            <a dmx-bind:href="/blog/@{{data_detail.data.author}}/{{data_detail.data.permlink}}" class="text-white">
+              <h3 class="card-title mt-2 text-center text-capitalize p-2">{{blogResult[2].dluxLoad.data.title}}</h3>
+            </a> <img src="..."  alt="Card image cap" class="card-img-top" dmx-bind:src="{{data_detail.data.json_metadata.parseJSON().picFind()}}" />
+            <div class="card-body">
+              <p class="p-2">{{data_detail2.data.body.removeMD()}}</p>
+            </div>
+            <center>
+              <a dmx-bind:href="{{data_detail.data.url}}" type="button" class="btn btn-outline-danger mb-4 btn-launch">Launch App</a>
+            </center>
+            <div class="card-footer">
+              <div class="collapse" dmx-bind:id="vote{{post_id}}">
+                <form id="voteForm">
+                  <div class="d-flex align-items-center text-white-50">
+                    <div>
+                      <button type="button" class="btn btn-primary" dmx-bind:id="voteBtn{{post_id}}" dmx-bind:onclick="vote('{{comment.author}}','{{data_detail.data.comment.permlink}}','slider{{post_id}}')">100%</button>
+                      <button type="button" class="btn btn-secondary" data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{post_id}}"><span class="close text-white">×</span></button>
+                    </div>
+                    <div class="flex-fill mx-2">
+                      <p class="my-1">
+                        <input type="range" class="form-control-range" step="0.01" value="100" dmx-bind:id="slider{{post_id}}" dmx-bind:onchange="updateVoteSubmit('voteBtn{{post_id}}','slider{{post_id}}');">
+                      </p>
+                    </div>
+                    <p class="my-0"><span class="mr-1"  id="commentVal">0</span><i class="ml-1 fab fa-fw fa-hive"></i></p>
+                  </div>
+                </form>
+              </div>
+              <div class="d-flex align-items-center my-2">
+                <div><a data-toggle="collapse" dmx-bind:data-target="{{&quot;#&quot;}}vote{{post_id}}"><i class="fas fa-heart mr-1"></i></a>{{data_detail.data.active_votes.countUpVotes()}} <i class="fas fa-comment ml-2 mr-1"></i>{{data_detail.data.children}}</div>
+                <div class="ml-auto"> {{data_detail.data.total_payout_value}}<i class="ml-1 fab fa-fw fa-hive"></i></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </dmx-data-detail>
+  </dmx-data-detail>
+  </div>
 	</div>
 	</div>
 </main>
