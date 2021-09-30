2a83118b87467913193d33b8e34d7531457938d6
Steven Ettinger <sjettingerjr@gmail.com>
2021-09-30 17:08:44 -0300

    price

--- public/js/dex.js
+++ public/js/dex.js
@@ -928,13 +928,13 @@ function  countdown(t, s, u){
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById(`timer-${s}-${u}`).innerHTML = days + "d " + hours + "h "
+  document.getElementById(`timer-${s}-${u}`).innerText = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById(`timer-${s}-${u}`).innerHTML = "EXPIRED";
+    document.getElementById(`timer-${s}-${u}`).innerText = "EXPIRED";
   }
 }, 1000);
 }
