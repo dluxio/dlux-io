bcaee3e4869485dd9834f9366f04ec3a3c9938b0
Mark <m.e.giles@gmail.com>
2021-10-01 08:21:52 -0700

    Update index.php

--- App/Views/dex/index.php
+++ App/Views/dex/index.php
@@ -43,13 +43,13 @@
 		  <p class="lead">The most decentralized token powering the multiverse!</p>
 		  <p class="lead"> <a class="btn btn-primary btn-lg" href="/dex/dlux/" role="button">Get DLUX</a> </p>
 	  </div>
-		<div style="border-bottom: #FFFFFF thick solid"><h1 class="text-white p-0 m-0">Fungible + Sealed Mint</h1></div>
+		<div style="border-bottom: #FFFFFF thick solid"><h1 class="text-white p-0 m-0">Fungible + Mint</h1></div>
 	   			<div class="card-columns my-3" id="sales-mint-cards" >
 					<div dmx-repeat:repeatsalesmint1="salesMint.data">
 					<div class="card" style="color:black; background:radial-gradient(gold,orange)">
 					<a href="#inventoryModal" class="a-1" data-toggle="modal" dmx-on:click="inventory_iterator.select($index);inventory_detail.select(uid)">
 						<div class="card-header text-center" style=""><h5 class="card-title" style="color: black">COIN</h5></div>
-						<div class="card-img-top" alt="dlux-coin"><h1 class="py-3 mb-0 text-center text-primary"><i class="fas fa-coins"></i></h1><h3 class="text-center mb-0" style="color: black;">DLUX Token</h3><p class="card-text text-center pt-1" style="color: black">Powering the multiverse!</p></div>
+						<div class="card-img-top" alt="dlux-coin"><h1 class="py-3 mb-0 text-center text-primary"><i class="fas fa-coins"></i></h1><h3 class="text-center mb-0" style="color: black;">DLUX Token</h3><p class="card-text lead text-center pt-1" style="color: black">Powering the multiverse!</p></div>
 						<div class="card-body" style="color: black;"><div class="text-center">0.254 HIVE</div></div>
 					</a>
 						<div class="card-footer text-center"><p class="lead"> <a class="btn btn-primary btn-lg" href="/dex/dlux/" role="button">Get DLUX</a> </p></div>
@@ -57,7 +57,7 @@
 					<div class="card text-white bg-dark" style="color:black; background:radial-gradient(lawngreen,#43C45F)" dmx-repeat:repeatsalesmint2="$value">
 					<a href="#inventoryModal" class="a-1" data-toggle="modal" dmx-on:click="inventory_iterator.select($index);inventory_detail.select(uid)">
 						<div class="card-header text-center" style="background: linear-gradient(#43C45F,lawngreen)"><h5 class="card-title" style="color: black">MINT</h5></div>
-						<div class="card-img-top" dmx-bind:id="image-{{set}}" dmx-bind:alt="image-{{set}}"><h1 class="py-3 mb-0 text-center text-primary" style="color: #43C45F"><i class="far fa-gem"></i></h1><h3 class="text-center mb-0" style="color: black;">{{set}} NFT</h3><p class="card-text text-center pt-1" style="color: black">Unwrap to see attributes.</p></div>
+						<div class="card-img-top" dmx-bind:id="image-{{set}}" dmx-bind:alt="image-{{set}}"><h1 class="py-3 mb-0 text-center text-primary" style="color: #43C45F"><i class="far fa-gem"></i></h1><h3 class="text-center mb-0" style="color: black;">{{set}} NFT</h3><p class="lead card-text text-center pt-1" style="color: black">Unwrap to see attributes.</p></div>
 						<div class="card-body" style="color: black;"><div class="text-center">{{items[0].price.nai()}}</div></div>
 					</a>
 						<div class="card-footer text-center"><p class="lead"> <a class="btn btn-primary btn-lg" href="/dex/dlux/" role="button">Buy NFT</a> </p></div>
