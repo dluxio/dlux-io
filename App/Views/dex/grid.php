5ac3de266b3b6231a3f5f978fda265dc61eca389
Mark <m.e.giles@gmail.com>
2021-10-06 16:43:25 -0700

    Update grid.php

--- App/Views/dex/grid.php
+++ App/Views/dex/grid.php
@@ -5,97 +5,6 @@
 <meta charset="utf-8">
 <title>Order Book</title>
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-<style>
-body {
-	padding-top: 80px;
-	background: #f2f2f2;
-	
-
-    font-family: 'Open Sans', sans-serif;
-	font-weight: 400;
-
-
-}
-.side-menu .nav-container {
- background: #fff;
-	border: 1px solid #e9e9e9;
-}
-.side-menu a{
-	text-transform: uppercase;
-	font-size: 12px;
-	color: #777;
-}
-.side-menu li.active a{
-	color: #444;
-	background: #f7f7f7;
-}
-.table {
-	background: #fff;
-	table-layout: fixed; 
-}
-.table-bordered {
-	border: 1px solid #e9e9e9;
-}
-.table > thead > tr > th, .table > tbody > tr > th, .table > tfoot > tr > th, .table > thead > tr > td, .table > tbody > tr > td, .table > tfoot > tr > td {
-	vertical-align:middle;
-}
-.table>tbody>tr>th, .table>tfoot>tr>th, .table>tbody>tr>td, .table>tfoot>tr>td {
-	padding: 12px 15px;
-}
-.table-bordered > thead > tr > th, .table-bordered > tbody > tr > th, .table-bordered > tfoot > tr > th, .table-bordered > thead > tr > td, .table-bordered > tbody > tr > td, .table-bordered > tfoot > tr > td {
-	border-color: #e9e9e9;
-}
-.pager li > a {
-	border-radius: 0;
-	background: #fff;
-	border: 1px solid #e9e9e9;
-	color: #777;
-    
-}
-.pager li > a:hover {
-	color: #444;
-	background: #f7f7f7; 
-}
-th {
-	position: relative;
-}
-.sort-btns {
-	position: absolute;
-	right: 5px;
-	top: 5px;
-}
-.sort-btns .btn {
-		font-size: 12px;
-	padding: 2px 8px;
-}
-.panel {
-	margin-top: 20px;
-	border: 1px solid #e9e9e9;
-}
-.panel-default > .panel-heading {
-	    background: #f7f7f7;
-
-}
-.panel-title {
-	    color: #444;
-	font-size: 14px;
-	margin: 0;
-	font-weight: 500;
-}
-.list-group-item {
-	font-size: 14px;
-	font-weight: 400;
-}
-tr.customer {
-	cursor: pointer;
-}
-
-</style>
-
-
-
-	
 <?php 
    $path = $_SERVER['DOCUMENT_ROOT'];
    $path .= "/mod/header.php";
@@ -115,75 +24,76 @@ tr.customer {
 ?>
 <body is="dmx-app" class="d-flex flex-column h-100 padme-t70 text-white ">
 <dmx-api-datasource id="api1" is="dmx-fetch" url="https://token.dlux.io/dex/"></dmx-api-datasource>
-<dmx-data-view id="dataView1" dmx-bind:data="api1.data.markets.hive.sellOrders" pagesize="15"></dmx-data-view>
+<dmx-data-view id="dexSellOrders" dmx-bind:data="api1.data.markets.hive.sells" pagesize="15"></dmx-data-view>
 <main role="main" class="flex-shrink-0">
 <section class="content">
   <div class="container">
-    <div class="row">
-      <div class="col-md-3">
-        <dmx-data-detail id="dataDetail1" dmx-bind:data="dataView1.data" key="$value.key">
-        	
-        	<div class="panel panel-default mt-0">
-         <div class="panel-heading">
-           <h3 class="panel-title">DETAIL</h3>
-         </div>
-         <ul class="list-group text-dark">
-  			<li class="list-group-item">DLUX&#58; {{dataDetail1.data.$value.amount}}</li>
-  			<li class="list-group-item">HIVE&#58; {{dataDetail1.data.$value.hive}}</li>
-  			<li class="list-group-item">RATE&#58; {{dataDetail1.data.$value.rate}} </li>
-  			<li class="list-group-item text-break">TXID&#58; {{dataDetail1.data.$value.txid}} </li>
-			 <li class="list-group-item text-break">KEY &#40;RATE&#58;TXID&#41;&#58; {{dataDetail1.data.$value.key}} </li>
-		</ul>
-       </div>
+  <div class="jumbotron bg-dark my-4 p-2">
+		<div class="jumbotron bg-darker mt-0 mb-2">
+			<h1>Data View</h1>
+		<div id="dexSellOrdersTable">
+			  <table class="table table-hover table-bordered table-dark table-striped text-white">
+			  	<thead>
+			  		<tr>
+						<th scope="col" dmx-class:table-primary="dexSellOrders.sort.on == 'rate'" >
+							<div class="d-flex align-items-end">
+							<p class="m-0 p-0">RATE</p> 
+							<span class="ml-4">
+                  			<button title="Sort Ascending" type="button" class="btn btn-sm btn-light" dmx-on:click="dexSellOrders.sort('rate','asc')" dmx-class:bg-primary="dexSellOrders.sort.dir == 'asc' && dexSellOrders.sort.on == 'rate'"><i class="fas fa-sort-amount-down-alt"></i></button>
+                  			<button title="Sort Descending" type="button" class="btn btn-sm btn-light" dmx-on:click="dexSellOrders.sort('rate','desc')" dmx-class:bg-primary="dexSellOrders.sort.dir == 'desc' && dexSellOrders.sort.on == 'rate'"><i class="fas fa-sort-amount-down"></i></button>
+                			</span>
+							</div>
+						</th>
+						<th scope="col" dmx-class:table-primary="dexSellOrders.sort.on == 'amount'">
+							<div class="d-flex align-items-end">
+							<p class="m-0 p-0">DLUX</p>
+							<span class="ml-4">
+                  			<button title="Sort Ascending" type="button" class="btn btn-sm btn-light"  dmx-on:click="dexSellOrders.sort('amount','asc')" dmx-class:bg-primary="dexSellOrders.sort.dir == 'asc' && dexSellOrders.sort.on == 'amount'"><i class="fas fa-sort-amount-down-alt"></i></button>
+                  			<button title="Sort Descending" type="button" class="btn btn-sm btn-light" dmx-on:click="dexSellOrders.sort('amount','desc')" dmx-class:bg-primary="dexSellOrders.sort.dir == 'desc' && dexSellOrders.sort.on == 'amount'"><i class="fas fa-sort-amount-down"></i></button>
+                			</span>
+						</th>
+                		<th scope="col" dmx-class:table-primary="dexSellOrders.sort.on == 'hive'">
+							<div class="d-flex align-items-end">
+							<p class="m-0 p-0">HIVE</p>
+							<span class="ml-4">
+                  			<button title="Sort Ascending" type="button" class="btn btn-sm btn-light" dmx-on:click="dexSellOrders.sort('hive','asc')" dmx-class:bg-primary="dexSellOrders.sort.dir == 'asc'  && dexSellOrders.sort.on == 'hive'"><i class="fas fa-sort-amount-down-alt"></i></button>
+                  			<button title="Sort Descending" type="button" class="btn btn-sm btn-light" dmx-on:click="dexSellOrders.sort('hive','desc')" dmx-class:bg-primary="dexSellOrders.sort.dir == 'desc'  && dexSellOrders.sort.on == 'hive'"><i class="fas fa-sort-amount-down"></i></button>
+               				</span>
+						</th>
+					</tr>
+					
+			  </thead>
+			  <tbody>
+			  	<tr dmx-repeat:repeat1="dexSellOrders.data" dmx-on:click="iteratedluxsellorders.select($index); dexSellOrdersDetail.select(txid)" dmx-class:table-primary="iteratedluxsellorders.value.txid ==  txid">
+						<td>{{rate}}</td>
+						<td>{{amount}}</td>
+						<td>{{hive}}</td>
+					</tr>
+				  
+			</tbody>
+			</table> 
+		</div>
+			</div>
+		<div class="jumbotron bg-darker mt-2 mb-0">
+			<h1>Data Iterator</h1>
+		<div id="dexSellOrdersIterate" class="d-flex justify-content-between align-items-center">
+			<button class="btn btn-secondary" dmx-on:click="iteratedluxsellorders.prev()">&larr;</button>
+			<dmx-data-iterator id="iteratedluxsellorders" dmx-bind:data="dexSellOrders.data" loop="true"><a href="#" dmx-on:click="dexSellOrdersDetail.select(value.txid)"><h3>{{index}} | {{value.amount}} DLUX | {{value.hive}} HIVE | {{value.rate}} RATE | {{value.fee}} FEE</h3>
+				<h5 class="text-break">{{value.txid}}</h5></a></dmx-data-iterator>
+			<button class="btn btn-secondary" dmx-on:click="iteratedluxsellorders.next()">&rarr;</button>
+		</div>
+		</div>
+	  <div class="jumbotron bg-darker my-2">
+			<h1>Data Detail</h1>
+		<dmx-data-detail id="dexSellOrdersDetail" dmx-bind:data="dexSellOrders.data" key="txid">
+			<h3>RATE&#58; {{dexSellOrdersDetail.data.rate}}</h3>
+			<h3>DLUX&#58; {{dexSellOrdersDetail.data.amount}}</h3>
+			<h3>HIVE&#58; {{dexSellOrdersDetail.data.hive}}</h3>
+			<h3>FEE&#58; {{dexSellOrdersDetail.data.fee}}</h3>
+		 <small class="text-break text-muted">TXID&#58; {{dexSellOrdersDetail.data.txid}}</small>
         </dmx-data-detail>
-      </div>
-      <div class="col-md-9">
-        <div class="table-responsive">
-          <table class="table table-bordered table-hover">
-            <thead>
-              <tr>
-            	<th>&#36;value</th>
-                <th dmx-class:success="dataView1.sort.on == '$value.amount'"> DLUX <span class="sort-btns">
-                  <button type="button" class="btn btn-xs btn-default" title="Sort Ascending" dmx-on:click="dataView1.sort('$value.amount','asc')"><i class="fa fa-sort-alpha-asc"></i></button>
-                  <button title="Sort Descending" type="button" class="btn btn-xs btn-default" dmx-on:click="dataView1.sort('$value.amount','desc')"><i class="fa fa-sort-alpha-desc"></i></button>
-                </span></th>
-                <th dmx-class:success="dataView1.sort.on == '$value.hive'"> HIVE <span class="sort-btns">
-                  <button title="Sort Ascending" type="button" class="btn btn-xs btn-default" dmx-on:click="dataView1.sort('$value.hive','asc')"><i class="fa fa-sort-alpha-asc"></i></button>
-                  <button title="Sort Descending" type="button" class="btn btn-xs btn-default" dmx-on:click="dataView1.sort('$value.hive','desc')"><i class="fa fa-sort-alpha-desc"></i></button>
-                </span></th>
-                <th dmx-class:success="dataView1.sort.on == 'rate'"> RATE <span class="sort-btns">
-                  <button title="Sort Ascending" type="button" class="btn btn-xs btn-default" dmx-on:click="dataView1.sort('rate','asc')"><i class="fa fa-sort-alpha-asc"></i></button>
-                  <button title="Sort Descending" type="button" class="btn btn-xs btn-default" dmx-on:click="dataView1.sort('rate','desc')"><i class="fa fa-sort-alpha-desc"></i></button>
-                </span></th>
-				  <th dmx-class:success="dataView1.sort.on == 'txid'"> TXID <span class="sort-btns">
-                  <button title="Sort Ascending" type="button" class="btn btn-xs btn-default" dmx-on:click="dataView1.sort('txid','asc')"><i class="fa fa-sort-alpha-asc"></i></button>
-                  <button title="Sort Descending" type="button" class="btn btn-xs btn-default" dmx-on:click="dataView1.sort('txid','desc')"><i class="fa fa-sort-alpha-desc"></i></button>
-                </span></th>
-				  <th dmx-class:success="dataView1.sort.on == 'key'"> KEY &#40;RATE&#58;TXID&#41;<span class="sort-btns">
-                  <button title="Sort Ascending" type="button" class="btn btn-xs btn-default" dmx-on:click="dataView1.sort('key','asc')"><i class="fa fa-sort-alpha-asc"></i></button>
-                  	<button title="Sort Descending" type="button" class="btn btn-xs btn-default" dmx-on:click="dataView1.sort('key','desc')"><i class="fa fa-sort-alpha-desc"></i></button>
-                </span></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="customer" dmx-repeat:repeat1="dataView1.data" dmx-on:click="dataDetail1.select($value.key)" dmx-class:success="dataDetail1.data.$value.key ==  $value.key">
-                <td>{{$value}}</td>
-				<td> {{$value.amount}} </td>
-                <td> {{$value.hive}} </td>
-                <td> {{$value.rate}} </td>
-				  <td class="text-break"> {{$value.txid}} </td>
-				  <td class="text-break"> {{$value.key}} </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <ul class="pager">
-          <li class="previous"> <a class="btn btn-default" href="javascript:void(0);" dmx-on:click="dataView1.prev()" dmx-show="dataView1.has.prev"><i class="fa fa-angle-left"></i></a></li>
-          <li>Page {{dataView1.page}} of {{dataView1.pages}}</li>
-          <li class="next"> <a class="btn btn-default" href="javascript:void(0)" dmx-on:click="dataView1.next()" dmx-show="dataView1.has.next"><i class="fa fa-angle-right"></i></a></li>
-        </ul>
-      </div>
-    </div>
+		</div>
+		</div>
   </div>
 </section>
 	</main>
