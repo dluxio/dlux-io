d5143f2c6a50a11df1d2b2f351c6bfda50ae9662
Mark <m.e.giles@gmail.com>
2021-10-14 20:26:39 -0700

    Update index.php

--- App/Views/me/index.php
+++ App/Views/me/index.php
@@ -68,7 +68,7 @@
 	else{
 		echo "<dmx-api-datasource id=\"dluxGetBlog\" is=\"dmx-fetch\" url=\"https://token.dlux.io/api/condenser_api/get_discussions_by_blog/\" dmx-param:tag=\"'robotolux'\" dmx-param:limit=\"15\"></dmx-api-datasource>";
 		echo "<dmx-api-datasource id=\"dluxGetAccount\" is=\"dmx-fetch\" url=\"https://token.dlux.io/api/condenser_api/get_accounts\" dmx-param:0=\"'robotolux'\"></dmx-api-datasource>";
-		echo "<dmx-api-datasource id=\"inventorydata\" is=\"dmx-fetch\" url=\"https://token.dlux.io/api/nfts/disregardfiat\"></dmx-api-datasource>";
+		echo "<dmx-api-datasource id=\"inventorydata\" is=\"dmx-fetch\" url=\"https://token.dlux.io/api/nfts/robotolux\"></dmx-api-datasource>";
         }
 ;?>
 
