a3a00604b0a06e5c03c0117609823a8e31cc4fa1
Mark <m.e.giles@gmail.com>
2021-10-20 01:13:02 -0700

    Update index.php

--- App/Views/nfts/set/index.php
+++ App/Views/nfts/set/index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en" class="h-100">
 <head>
-<title>DLUX - DEX</title>
+<title>DLUX - NFTs</title>
 <?php
 $path = $_SERVER[ 'DOCUMENT_ROOT' ];
 $path .= "/mod/header.php";
