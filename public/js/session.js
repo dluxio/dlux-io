a3c59a76f839394140cc041cb1a628ea370ffcb0
Steven Ettinger <sjettingerjr@gmail.com>
2021-11-04 02:02:43 -0300

    ft trades

--- public/js/session.js
+++ public/js/session.js
@@ -606,7 +606,7 @@ function buyFT(setname, uid, price,  callback){
 
 function bidFT(setname, uid, callback){
     const bid_amount = document.getElementById(`${setname}-${uid}-bid`).value
-    console.log({bid_id, bid_amount})
+    console.log({bid_amount})
     bid_amount = parseInt(bid_amount * 1000)
     broadcastCJA({set: setname, uid, bid_amount}, 'dlux_ft_bid', `Trying to bid on ${setname} mint token.`) 
  }
