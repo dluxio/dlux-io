cadff88457e7020b9a3e4669af7e976d157579fd
Mark <m.e.giles@gmail.com>
2021-11-14 12:19:22 -0800

    Fix

---
+++ public/App/Controllers/Honeycombx.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+/**
+ * Home controller
+ *
+ * PHP version 7.0
+ */
+class Honeycomb extends \Core\Controller
+{
+
+    /**
+     * Show the index page
+     *
+     * @return void
+     */
+    public function index($request, $response, $service)
+    {
+        View::render('honeycomb/index.php');
+    }
+}
+
