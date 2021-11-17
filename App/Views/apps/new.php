fdf8041a76e125acfbcbba57501687475c66e37d
Mark <m.e.giles@gmail.com>
2021-11-16 17:37:01 -0800

    Update new.php

--- App/Views/apps/new.php
+++ App/Views/apps/new.php
@@ -7,19 +7,24 @@
    $path .= "/mod/header.php";
    include_once($path);
 ?>
-
+<script type="text/javascript" src="/dlux-io/dmxAppConnect/dmxAppConnect.js"></script>
+<script type="text/javascript" src="/dlux-io/dmxAppConnect/dmxDataTraversal/dmxDataTraversal.js"></script>
 </head>
 
 <body class="d-flex flex-column h-100 padme-t70" id="index" is="dmx-app">
 <button id="btn1" dmx-on:click="data_view1.pageSize(1)">Button</button>
 <dmx-api-datasource id="api1" is="dmx-fetch" url="https://dluxdata.herokuapp.com/new"></dmx-api-datasource>
 <dmx-data-view id="data_view1" dmx-bind:data="api1.data.result"></dmx-data-view>
-<div dmx-repeat:repeat1="data_view1.data" class="text-white"> {{$index}}
-  {{$key}}
+<div dmx-repeat:repeat1="data_view1.data" class="text-white">
+  {{$index}}
   {{author}}
   {{permlink}}
   {{block}}
-	
+  {{votes}}
+  {{voteweight}}
+  {{promote}}
+  {{paid}}
+  {{url}}
 	</div>
 <?php 
    $path = $_SERVER['DOCUMENT_ROOT'];
