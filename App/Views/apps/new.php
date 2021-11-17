80d9f3b1268585afc3b4ca66f30d04aebf6bec0b
Mark <m.e.giles@gmail.com>
2021-11-16 17:43:52 -0800

    Update new.php

--- App/Views/apps/new.php
+++ App/Views/apps/new.php
@@ -12,7 +12,7 @@
 </head>
 
 <body class="d-flex flex-column h-100 padme-t70" id="index" is="dmx-app">
-<button id="btn1" dmx-on:click="data_view1.setPageSize('1')">pageSize(1)</button>
+<button id="btn1" dmx-on:click="data_view1.setPageSize('1')">pageSize('1')</button>
 <button id="btn2" dmx-on:click="data_view1.sort('author','asc')">sort('author','asc')</button>
 <dmx-api-datasource id="api1" is="dmx-fetch" url="https://dluxdata.herokuapp.com/new"></dmx-api-datasource>
 <dmx-data-view id="data_view1" dmx-bind:data="api1.data.result"></dmx-data-view>
