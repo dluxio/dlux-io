59bc356594d52cafb363d39cb39e205e0839bfa4
Mark <m.e.giles@gmail.com>
2021-11-16 17:44:04 -0800

    Update new.php

--- App/Views/apps/new.php
+++ App/Views/apps/new.php
@@ -7,8 +7,7 @@
    $path .= "/mod/header.php";
    include_once($path);
 ?>
-<script type="text/javascript" src="/dlux-io/dmxAppConnect/dmxAppConnect.js"></script>
-<script type="text/javascript" src="/dlux-io/dmxAppConnect/dmxDataTraversal/dmxDataTraversal.js"></script>
+
 </head>
 
 <body class="d-flex flex-column h-100 padme-t70" id="index" is="dmx-app">
