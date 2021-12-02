ef3473fe20753e45c9aa55c069c71e19fe15ebbd
Steven Ettinger <sjettingerjr@gmail.com>
2021-12-02 18:03:50 -0300

    new chart

--- public/js/chart.js
+++ public/js/chart.js
@@ -49,9 +49,9 @@ function getHistorical(pair, width, bc){
                     current.c = dex.markets[pair.toLowerCase()].days[buckets[i]].c
                     current.v += dex.markets[pair.toLowerCase()].days[buckets[i]].v
                 } else {
-                    current.o = dex.markets[pair.toLowerCase()].days[buckets[i]].c
-                    current.h = dex.markets[pair.toLowerCase()].days[buckets[i]].c
-                    current.l = dex.markets[pair.toLowerCase()].days[buckets[i]].c
+                    current.o = dex.markets[pair.toLowerCase()].days[buckets[i]].o
+                    current.h = dex.markets[pair.toLowerCase()].days[buckets[i]].h
+                    current.l = dex.markets[pair.toLowerCase()].days[buckets[i]].l
                     current.c = dex.markets[pair.toLowerCase()].days[buckets[i]].c
                     current.v = dex.markets[pair.toLowerCase()].days[buckets[i]].v
                 }
@@ -63,6 +63,46 @@ function getHistorical(pair, width, bc){
                     current.l = current.c
                     current.v = 0
                 } else if (!buckets[i+1]) {
+                    bars.push({x: currentBucket, o: current.o, h: current.h, l: current.l, c: current.c, v: current.v})
+                }
+            } else {
+                console.log(new Date(now - (3000 * (current_block - parseInt(buckets[i])))), new Date(currentBucket))
+            }
+        }
+        let items = Object.keys(dex.markets[pair.toLowerCase()].his)
+        for (var i = 0; i < items.length; i++) {
+            if (new Date(now - (3000 * (current_block - parseInt(items[i].split(':')[0])))).getTime() > currentBucket) {
+                if(!bars.length){
+                    while(new Date(now - (3000 * (current_block - parseInt(items[i].split(':')[0])))).getTime() > currentBucket + period){
+                        bars.push({x: currentBucket, o: 0, h: 0, l: 0, c: 0, v: 0})
+                        currentBucket = new Date(currentBucket + period).getTime()
+                    }
+                } else {
+                    while(new Date(now - (3000 * (current_block - parseInt(items[i].split(':')[0])))).getTime() > currentBucket + period){
+                        bars.push({x: currentBucket, o: bars[bars.length - 1].c, h: bars[bars.length - 1].c, l: bars[bars.length - 1].c, c: bars[bars.length - 1].c, v: 0})
+                        currentBucket = new Date(currentBucket + period).getTime()
+                    }
+                }
+                if(current.o){
+                    if(dex.markets[pair.toLowerCase()].his[items[i]].price > current.h)current.h = dex.markets[pair.toLowerCase()].his[items[i]].price
+                    if(dex.markets[pair.toLowerCase()].his[items[i]].price < current.l)current.l = dex.markets[pair.toLowerCase()].his[items[i]].price
+                    current.c = dex.markets[pair.toLowerCase()].his[items[i]].price
+                    current.v += dex.markets[pair.toLowerCase()].his[items[i]].target_vol
+                } else {
+                    current.o = dex.markets[pair.toLowerCase()].his[items[i]].price
+                    current.h = dex.markets[pair.toLowerCase()].his[items[i]].price
+                    current.l = dex.markets[pair.toLowerCase()].his[items[i]].price
+                    current.c = dex.markets[pair.toLowerCase()].his[items[i]].price
+                    current.v = dex.markets[pair.toLowerCase()].his[items[i]].varget_vol
+                }
+                if(items[i+1] && new Date(now - (3000 * (current_block - parseInt(items[i+1].split(':')[0])))).getTime() > currentBucket + period){
+                    bars.push({x: currentBucket, o: current.o, h: current.h, l: current.l, c: current.c, v: current.v})
+                    currentBucket = new Date(currentBucket + period).getTime()
+                    current.o = 0
+                    current.h = current.c
+                    current.l = current.c
+                    current.v = 0
+                } else if (!items[i+1]) {
                     bars.push({x: currentBucket, o: current.o, h: current.h, l: current.l, c: current.c, v: current.v})
                 }
             } else {
