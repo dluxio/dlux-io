c02a445a1131b39ea8d6cb2542db3e682db69a15
Steven Ettinger <sjettingerjr@gmail.com>
2021-12-02 00:39:19 -0300

    concat

--- public/js/chart.js
+++ public/js/chart.js
@@ -16,7 +16,9 @@ function getHistorical(pair, width, bc){
     fetch(`https://token.dlux.io/api/historical/${pair.toUpperCase()}_DLUX?depth=200`)
     .then(res => res.json())
     .then(data => {
+        console.log(data.sell,data.buy)
         var arr = [...data.sell, ...data.buy]
+        console.log(arr)
         arr.sort((x, y) => { return x.trade_timestamp - y.trade_timestamp })
         console.log(arr)
         var bars = []
