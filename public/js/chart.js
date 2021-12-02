9e5aec225bb7c44a6351abed216de1f767a6f6e2
Steven Ettinger <sjettingerjr@gmail.com>
2021-12-02 17:04:44 -0300

    new chart

--- public/js/chart.js
+++ public/js/chart.js
@@ -22,7 +22,7 @@ function getHistorical(pair, width, bc){
     ).then(jsons => {
         const now = new Date()
         console.log({now})
-        var startdate = Date(now.valueOf() - (period * numbars))
+        var startdate = Date(now.getTime() - (period * numbars))
         var currentBucket = startdate
         const dex = jsons[1], recent = jsons[0]
         const current_block = dex.stats.lastIBlock
@@ -30,17 +30,17 @@ function getHistorical(pair, width, bc){
         buckets.sort(function (a,b){return parseInt(a) - parseInt(b)})
         var bars = []
         for (var i = 0; i < buckets.length; i++) {
-            console.log(Date(now.valueOf() - (3000 * (current_block - parseInt(buckets[i])))).valueOf(), startdate.valueOf())
-            if (Date(now.valueOf() - (3000 * (current_block - parseInt(buckets[i])))) > startdate.valueOf()) {
+            console.log(Date(now.getTime() - (3000 * (current_block - parseInt(buckets[i])))).getTime(), startdate.getTime())
+            if (Date(now.getTime() - (3000 * (current_block - parseInt(buckets[i])))) > startdate.getTime()) {
                 if(!bars.length){
-                    while(Date(now.valueOf() - (3000 * (current_block - parseInt(buckets[i]))).valueOf()) > currentBucket.valueOf() + period){
+                    while(Date(now.getTime() - (3000 * (current_block - parseInt(buckets[i]))).getTime()) > currentBucket.getTime() + period){
                         bars.push({x: currentBucket, o: 0, h: 0, l: 0, c: 0, v: 0})
-                        currentBucket = new Date(currentBucket.valueOf() + period)
+                        currentBucket = new Date(currentBucket.getTime() + period)
                     }
                 } else {
-                    while(Date(now.valueOf() - (3000 * (current_block - parseInt(buckets[i]))).valueOf()) > currentBucket.valueOf() + period){
+                    while(Date(now.getTime() - (3000 * (current_block - parseInt(buckets[i]))).getTime()) > currentBucket.getTime() + period){
                         bars.push({x: currentBucket, o: bars[bars.length - 1].c, h: bars[bars.length - 1].c, l: bars[bars.length - 1].c, c: bars[bars.length - 1].c, v: 0})
-                        currentBucket = new Date(currentBucket.valueOf() + period)
+                        currentBucket = new Date(currentBucket.getTime() + period)
                     }
                 }
                 bars.push({x: currentBucket, o: dex.markets[pair.toLowerCase()].days[buckets[i]].o, h: dex.markets[pair.toLowerCase()].days[buckets[i]].t, l: dex.markets[pair.toLowerCase()].days[buckets[i]].b, c: dex.markets[pair.toLowerCase()].days[buckets[i]].c, v: dex.markets[pair.toLowerCase()].days[buckets[i]].v})
