ba6820159951275633663262fd9c4a94bfc83e32
Mark <m.e.giles@gmail.com>
2021-12-03 11:09:46 -0800

    Update index.php

--- App/Views/dex/index.php
+++ App/Views/dex/index.php
@@ -58,8 +58,8 @@ if ( isset( $_COOKIE[ 'user' ] ) ) {
 			</div>
 			<div class="d-flex text-white-50">
 			<div id="userdlux" class="mx-4 text-warning">{{(openordersapi.data.balance/1000).formatNumber(3,'.',',')}} DLUX</div>
-          <div id="userdpwr" class="mx-4 text-primary">{{(openordersapi.data.poweredUp/1000).formatNumber(3,'.',',')}} DPWR</div>
-          <div id="userdgov" class="mx-4 text-info">{{(openordersapi.data.gov/1000).formatNumber(3,'.',',')}} DGOV</div>
+          <div id="userdpwr" class="mx-4 text-info">{{(openordersapi.data.poweredUp/1000).formatNumber(3,'.',',')}} DPWR</div>
+          <div id="userdgov" class="mx-4 text-primary">{{(openordersapi.data.gov/1000).formatNumber(3,'.',',')}} DGOV</div>
           <div id="userhive" class="mx-4 text-danger">{{accountapi.data.result[0].balance}}</div>
           <div id="userhive" class="mx-4 text-success">{{accountapi.data.result[0].hbd_balance}}</div>
           </div>
