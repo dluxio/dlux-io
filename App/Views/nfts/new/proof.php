2f0644f286bb186251528bbb2b61bbe8ea653f7c
Mark <m.e.giles@gmail.com>
2021-12-07 22:38:35 -0800

    Update proof.php

--- App/Views/nfts/new/proof.php
+++ App/Views/nfts/new/proof.php
@@ -22,6 +22,14 @@ include_once( $path );
     height: 100%; 
     object-fit: contain;
 }
+		.attribute-name {
+		text-decoration: underline;
+		margin-top: 5px;
+	}
+	.attribute-value {
+		font-weight: bold;
+		margin-bottom: 5px;
+	}
 </style>
 <dmx-api-datasource id="statsapi" is="dmx-fetch" url="https://token.dlux.io/stats/"></dmx-api-datasource>
 <?php
@@ -30,67 +38,58 @@ $path .= "/mod/nav.php";
 include_once( $path );
 ?>
 <main role="main" class="flex-shrink-0">
-<div class="container">
-  <div class="container-fluid bg-dark rounded my-5 py-3">
-	  <h1>PROOF NFT SET</h1>
-		<h4>ALPHA v0.2</h4>
-    <p class="lead">Proof your set before publishing</p>
-
-	<div id="preview">
-
-  <div class="form-group row">
-    <label for="testscript" class="col-sm-2 col-form-label">Script</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="testscript" value="QmSPm13knazJsN4C8b7mWqT8tG2CeFCRvbW1PifYZV9dVN">
-    </div>
-  </div>
-
-			<div dmx-bind:id="{{testscript.value}}banner"> {{testscript.value.getSetPhotos('','banner')}}</div>
-		
-		<div>	
-		<div dmx-bind:id="{{testscript.value}}featured"> {{testscript.value.getSetPhotos('','featured')}}</div>
-			<div class="arrow2 rounded-circle border border-warning bg-darker text-warning">
-                   <div dmx-bind:id="{{testscript.value}}logo" class="rounded-circle" style="width: 100px"> {{testscript.value.getSetPhotos('','logo','rounded-circle')}}</div>
-                  </div>
-		</div>
-			<di class="row">
-			<div class="col-md-6">
-				<div dmx-bind:id="{{testscript.value}}wrapped"> {{testscript.value.getSetPhotos('','wrapped')}}</div>
-				<p dmx-bind:id="{{testscript.value}}descriptionp"> {{testscript.value.getSetDetails('Description', 'descriptionp', 'innerText')}} </p>
-		 
- 			<input id="testuid" value="7S">
- 			<input id="testset" value="test">
-		</div>
-			<div class="col-md-6">
+  <div class="container">
+    <div class="container-fluid bg-dark rounded my-5 py-3">
+      <h1>PROOF NFT SET</h1>
+      <h4>ALPHA v0.2</h4>
+      <p class="lead">Proof your set before publishing</p>
+      <div id="preview">
+        <div class="form-group row">
+          <label for="testscript" class="col-sm-2 col-form-label">Script</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control" id="testscript" value="QmSPm13knazJsN4C8b7mWqT8tG2CeFCRvbW1PifYZV9dVN">
+          </div>
+        </div>
+        <div dmx-bind:id="{{testscript.value}}banner" class="my-3"> {{testscript.value.getSetPhotos('','banner','rounded')}}</div>
+        <div>
+          <div dmx-bind:id="{{testscript.value}}featured"> {{testscript.value.getSetPhotos('','featured')}}</div>
+          <div class="arrow2 rounded-circle border border-warning bg-darker text-warning">
+            <div dmx-bind:id="{{testscript.value}}logo" class="rounded-circle" style="width: 100px"> {{testscript.value.getSetPhotos('','logo','rounded-circle')}}</div>
+          </div>
+        </div>
+        <di class="row">
+        <div class="col-md-6">
+          <div dmx-bind:id="{{testscript.value}}wrapped"> {{testscript.value.getSetPhotos('','wrapped')}}</div>
+          <p dmx-bind:id="{{testscript.value}}descriptionp"> {{testscript.value.getSetDetails('Description', 'descriptionp', 'innerText')}} </p>
+          <input id="testuid" value="7S">
+          <input id="testset" value="test">
+        </div>
+        <div class="col-md-6">
           <div class="cc-3 pt-5" id="testftcards" >
-			 
             <div dmx-bind:id="{{testscript.value}}testcard" class="card text-white" style="border: none;"> {{testscript.value.getSetDetailsColors('testcard')}}
-              <div class="card-header d-flex align-items-center justify-content-between" >
-			 </div>
-                <div>
-                  <div class="card-img-top" dmx-bind:id="image-{{testset.value}}-{{testuid.value}}" dmx-bind:alt="{{testscript.value}}">{{testuid.value.nftImageWell(testscript.value, testset.value)}}</div>
-                   </div>
-             
+              <div class="card-header d-flex align-items-center justify-content-between" > </div>
+              <div>
+                <div class="card-img-top" dmx-bind:id="image-{{testset.value}}-{{testuid.value}}" dmx-bind:alt="{{testscript.value}}">{{testuid.value.nftImageWell(testscript.value, testset.value)}}</div>
+              </div>
               <div class="card-body text-center d-flex flex-column lead">
                 <div class="px-2 py-5 text-center rounded" style="background-color: rgba(0,0,0,0.75)">{{testscript.value.getSetDetailsIcon('testicon')}}
                   <h1 class="text-center rainbow-text"><i dmx-bind:id="{{testscript.value}}testicon"></i></h1>
+					<div dmx-bind:id="{{testscript.value}}-{{testuid.value}}-attributes" class="attribute-container">  {{testscript.value.getNFTDetails(testuid.value)}} </div>
                 </div>
               </div>
-             
             </div>
           </div>
-				</div>
-				</div>
-	</div>
-    <p>
- 
+        </div>
+      </div>
+    </div>
+    <p> 
   </div>
-  </main>
-  <?php
-  $path = $_SERVER[ 'DOCUMENT_ROOT' ];
-  $path .= "/mod/footer.php";
-  include_once( $path );
-  ?>
+</main>
+<?php
+$path = $_SERVER[ 'DOCUMENT_ROOT' ];
+$path .= "/mod/footer.php";
+include_once( $path );
+?>
 </div>
 </body>
 <script>
