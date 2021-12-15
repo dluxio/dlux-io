2f5c179ad77b90dd65e6f485d8b6f2e0893323b1
Mark <m.e.giles@gmail.com>
2021-12-14 19:08:25 -0800

    Update index.php

--- App/Views/dex/index.php
+++ App/Views/dex/index.php
@@ -332,7 +332,7 @@ if ( isset( $_COOKIE[ 'user' ] ) ) {
                   <legend tabindex="-1" class="col-sm-4 col-form-label" id="buy-hive-total-label">Total</legend>
                   <div tabindex="-1" role="group" class="col">
                     <div role="group" class="input-group">
-                      <input type="number" class="form-control" dmx-bind:value="buyPrice.value*buyQuantity.value" id="buyHiveTotal" placeholder="0" min="0.001" step="0.001" aria-required="true" dmx-bind:max="" dmx-bind:readonly="buylimit.checked">
+                      <input type="number" class="form-control" dmx-bind:value="buyPrice.value*buyQuantity.value" id="buyHiveTotal" placeholder="0" min="0.001" step="0.001" aria-required="true" dmx-bind:max="">
                       <div class="input-group-append">
                         <div class="input-group-text">HIVE</div>
                       </div>
@@ -346,7 +346,7 @@ if ( isset( $_COOKIE[ 'user' ] ) ) {
                   <legend tabindex="-1" class="col-sm-4 col-form-label" id="buy-hbd-total-label">Total</legend>
                   <div tabindex="-1" role="group" class="col">
                     <div role="group" class="input-group">
-                      <input type="number" class="form-control" id="buyHBDTotal" dmx-bind:value="buyPrice.value*buyQuantity.value" placeholder="0" min="0.001" step="0.001" dmx-bind:max="" aria-required="true" dmx-bind:readonly="buylimit.checked">
+                      <input type="number" class="form-control" id="buyHBDTotal" dmx-bind:value="buyPrice.value*buyQuantity.value" placeholder="0" min="0.001" step="0.001" dmx-bind:max="" aria-required="true">
                       <div class="input-group-append">
                         <div class="input-group-text">HBD</div>
                       </div>
