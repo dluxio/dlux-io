38828e33520dd863e985941202eea6842d59b2e2
Mark <m.e.giles@gmail.com>
2021-12-17 22:18:22 -0800

    Update index.php

--- App/Views/nfts/index.php
+++ App/Views/nfts/index.php
@@ -982,7 +982,7 @@ include_once( $path );
               </div>
             </div>
 			  
-   
+   			 <button id="cancelNFTbtn1" class="btn btn-warning btn-lg" dmx-show="(by == userCookie.value)" dmx-on:click="sellNFTcancel('{{set}}','{{uid}}')">Cancel</button>
               <button class="btn btn-lg btn-info" dmx-show="(by != userCookie.value)" dmx-on:click="buyNFT('{{sales_detail.data.set}}','{{sales_detail.data.uid}}','{{sales_detail.data.price.amount}}','{{sales_detail.data.price.token}}')">Buy</button>
          
 			  
@@ -998,39 +998,7 @@ include_once( $path );
     </div>
   </div>
 </dmx-data-detail>
-  <dmx-data-detail id="sales_detail" dmx-bind:data="salesview.data" key="uid">
-  <div class="modal fade " id="salesModal" tabindex="11" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-full modal modal-dialog-centered" role="document">
-      <div class="modal-content bg-dark text-white">
-        <div class="card text-white bg-dark ">
-          <div class="card-header d-flex align-items-center justify-content-between" dmx-bind:id="{{sales_detail.data.script}}{{sales_detail.data.uid}}-nftsalesdetailheader">{{sales_detail.data.script.getSetDetailsColors(sales_detail.data.uid+'-nftsalesdetailheader')}}
-            <div class="rounded-pill d-flex align-items-center p-2" style="background-color: black">
-              <h2 class="m-0 px-2">{{sales_detail.data.uid}}</h2>
-            </div>
-            <h3 class="card-title lead border border-dark rounded mb-0 p-2"><a dmx-bind:href="/nfts/set/{{sales_detail.data.set}}" style="color: black">{{sales_detail.data.set}} NFT</a></h3>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          </div>
-        </div>
-        <div class="card-img-top" dmx-bind:id="detail-image-{{sales_detail.data.set}}-{{sales_detail.data.uid}}" dmx-bind:alt="{{sales_detail.data.set}}-{{sales_detail.data.uid}}">{{sales_detail.data.uid.nftDetailWell(sales_detail.data.script, sales_detail.data.set)}}</div>
-        <div class="card-body text-center">
-          <div>{{sales_detail.data.by}} is selling {{sales_detail.data.uid}}</div>
-          <div>Set: {{sales_detail.data.set}}</div>
-          <div>Price: {{sales_detail.data.price.nai()}}</div>
-          <form class="my-3">
-			  <button id="cancelNFTbtn1" class="btn btn-warning btn-lg" dmx-show="(by == userCookie.value)" dmx-on:click="sellNFTcancel('{{sales_detail.data.set}}','{{sales_detail.data.uid}}')">Cancel</button>
-            <button class="btn btn-lg btn-primary" dmx-show="(by != userCookie.value)" dmx-on:click="buyNFT('{{sales_detail.data.set}}','{{sales_detail.data.uid}}','{{sales_detail.data.price.amount}}')">Buy NFT</button>
-          </form>
-        </div>
-        <div class="card-footer d-flex align-items-center">
-          <h2><a class="text-muted p-3" href="#" dmx-on:click="sales_iterator.prev();sales_detail.select(sales_iterator.value.uid)"><i class="fas fa-caret-square-left"></i></a></h2>
-          <small class="ml-auto text-muted"><i>Item {{sales_iterator.index + 1}} of {{salesview.data.count()}}</i></small>
-          <h2 class="ml-auto"><a class="text-muted p-3" href="#" dmx-on:click="sales_iterator.next();sales_detail.select(sales_iterator.value.uid)"><i class="fas fa-caret-square-right"></i></a></h2>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-</dmx-data-detail>
+ 
 </main>
 <?php
 $path = $_SERVER[ 'DOCUMENT_ROOT' ];
