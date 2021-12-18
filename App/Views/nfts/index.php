c4ba440705ef7235610bd21c8483e1e0263f472d
Mark <m.e.giles@gmail.com>
2021-12-17 19:17:26 -0800

    Update index.php

--- App/Views/nfts/index.php
+++ App/Views/nfts/index.php
@@ -784,7 +784,7 @@ include_once( $path );
 <!-- NFT Auctions Detail -->
 <dmx-data-detail id="auctions_detail" dmx-bind:data="auctionsview.data" key="uid">
   <div class="modal fade " id="auctionsModal" tabindex="11" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-full modal modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-full modal-xl modal-dialog-centered" role="document">
       <div class="modal-content bg-dark text-white">
         <div class="card border-0 text-white bg-dark ">
           <div class="card-header border-0 d-flex align-items-center justify-content-between" dmx-bind:id="{{auctions_detail.data.script}}{{auctions_detail.data.uid}}-nftauctiondetailheader">{{auctions_detail.data.script.getSetDetailsColors(auctions_detail.data.uid+'-nftauctiondetailheader')}}
@@ -794,36 +794,38 @@ include_once( $path );
             <h3 class="card-title lead border border-dark rounded mb-0 p-2"><a dmx-bind:href="/nfts/set/{{auctions_detail.data.set}}" style="color: black">{{auctions_detail.data.set}} NFT</a></h3>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
           </div>
+			<div class="row">
+			<div class="col-md-6">
           <div class="card-img-top" dmx-bind:id="detail-image-{{auctions_detail.data.set}}-{{auctions_detail.data.uid}}" dmx-bind:alt="{{auctions_detail.data.set}}-{{auctions_detail.data.uid}}">{{auctions_detail.data.uid.nftDetailWell(auctions_detail.data.script, auctions_detail.data.set)}}</div>
           </div>
-          <div class="text-center " style="background: crimson">
-            <h5 dmx-bind:id="detail-timer-{{auctions_detail.data.set}}-{{auctions_detail.data.uid}}" class="mb-0 lead">{{auctions_detail.data.time.animateTime(auctions_detail.data.set, auctions_detail.data.uid, 1)}}</h5>
-          </div>
-          <div class="card-body text-center">
-			  
-			  <div dmx-bind:id="{{auctions_detail.data.script}}-header"  style="-webkit-background-clip: text;color: transparent;">{{auctions_detail.data.script.getSetDetailsColorsTxt('-header')}}
+			<div class="col-md-6">
+			<div dmx-bind:id="{{auctions_detail.data.script}}-header"  style="-webkit-background-clip: text;color: transparent;">{{auctions_detail.data.script.getSetDetailsColorsTxt('-header')}}
                   <div class="mt-3">
                     <h1 class="text-center">{{auctions_detail.data.name_long}}<i dmx-bind:id="{{auctions_detail.data.script}}icon" class="ml-3"></i></h1>
                     {{auctions_detail.data.script.getSetDetailsIcon('icon')}}</div>
                 </div>
-			  
-           <div dmx-bind:id="{{auctions_detail.data.script}}-{{auctions_detail.data.uid}}-attributes" class="d-flex flex-wrap"> {{auctions_detail.data.script.getNFTDetails(auctions_detail.data.uid)}} </div>
+				 <div dmx-bind:id="{{auctions_detail.data.script}}-{{auctions_detail.data.uid}}-attributes" class="d-flex flex-wrap"> {{auctions_detail.data.script.getNFTDetails(auctions_detail.data.uid)}} </div>
+			</div>
+				</div>
+				
+          <div class="text-center " style="background: crimson">
+            <h5 dmx-bind:id="detail-timer-{{auctions_detail.data.set}}-{{auctions_detail.data.uid}}" class="mb-0 lead">{{auctions_detail.data.time.animateTime(auctions_detail.data.set, auctions_detail.data.uid, 1)}}</h5>
+          </div>
+			
+          <div class="card-body text-center">
 			  <div class="d-flex justify-content-around">
-			  <div class="d-flex flex-column">
-              <div> <small>Seller:</small>
-                <h6><a dmx-bind:href="/@{{auctions_detail.data.by}}#inventory">{{auctions_detail.data.by}}</a></h6>
+
+				  <div><small><u>Seller</u></small>
+                <h6><a dmx-bind:href="/@{{auctions_detail.data.by}}#inventory">@{{auctions_detail.data.by}}</a></h6>
               </div>
-               <div> <small>High Bidder:</small>
-                <h5><a dmx-bind:href="/@{{auctions_detail.data.bidder}}#inventory">{{auctions_detail.data.bidder}}</a></h5>
-              </div>
-            </div>
-            <div class="d-flex flex-column">
-				<div> <small>Starting Price:</small>
+				<div><small><u>Starting Price</u></small>
                 <h6>{{auctions_detail.data.initial_price.nai()}}</h6>
               </div>
-             
-              <div> <small>Current Bid:</small>
-                <h5>{{auctions_detail.data.price.nai()}}</h5>
+				  <div><small><u>High Bidder</u></small>
+                <h6><a dmx-bind:href="/@{{auctions_detail.data.bidder}}#inventory">@{{auctions_detail.data.bidder}}</a></h6>
+              </div>
+              <div><small><u>Current Bid</u></small>
+                <h6>{{auctions_detail.data.price.nai()}}</h6>
               </div>
             </div>
 			  </div>
