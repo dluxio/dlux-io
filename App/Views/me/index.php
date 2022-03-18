de43e513258f9eb07359475c4cd84eca5271b709
Mark <m.e.giles@gmail.com>
2022-03-18 00:27:05 -0700

    Update index.php

--- App/Views/me/index.php
+++ App/Views/me/index.php
@@ -260,11 +260,10 @@ if ( isset( $author ) ) {
 					    <div class="input-group mb-3">
 					      <div class="input-group-prepend">
 					        <div class="input-group-text">
-					          <input id="govcheck" type="checkbox" aria-label="Claim GOV not PWR">
-
+					          <input id="govcheck" class="btn-outline-secondary" type="checkbox" aria-label="Claim GOV not PWR">
 				            </div>
 				          </div>
-					      <input type="text" placeholder="Claim GOV not PWR">
+					      <input type="text" class="bg-dark border-dark text-white form-control" placeholder="Claim GOV not PWR">
 				        </div>
                       </div>
                   </div>
