40422659fa567bc708db841c6b3564d6a6927b57
Mark <m.e.giles@gmail.com>
2022-03-27 11:40:55 -0700

    Update larynx.php

--- App/Views/dex/larynx.php
+++ App/Views/dex/larynx.php
@@ -97,15 +97,13 @@ include_once( $path );
             </div>
           </div>
           <div class="d-flex align-items-center text-white-50">
-			  <a href="/me#larynx">
-            <div id="userdlux" class="mx-4 text-warning">{{(openordersapi.data.balance/1000).formatNumber(3,'.',',')}} LARYNX</div>
-            <div id="userdpwr" class="mx-4 text-primary" dmx-show="openordersapi.data.poweredUp > 0">{{(openordersapi.data.poweredUp/1000).formatNumber(3,'.',',')}} LARYNXP</div>
-            <div id="userdgov" class="mx-4 text-info" dmx-show="openordersapi.data.gov > 0">{{(openordersapi.data.gov/1000).formatNumber(3,'.',',')}} LARYNXG</div>
-			</a>
-			  <a href="/me#hive">
-            <div id="userhive" class="mx-4 text-danger">{{accountapi.data.result[0].balance}}</div>
-            <div id="userhbd" class="mx-4 text-success">{{accountapi.data.result[0].hbd_balance}}</div>
-				  </a>
+			  
+            <div id="userdlux" class="mx-4 text-warning"><a href="/me#spk"><span class="text-warning">{{(openordersapi.data.balance/1000).formatNumber(3,'.',',')}} LARYNX</span></a></div>
+            <div id="userdpwr" class="mx-4 text-primary" dmx-show="openordersapi.data.poweredUp > 0"><a href="/me#spk"><span class="text-primary">{{(openordersapi.data.poweredUp/1000).formatNumber(3,'.',',')}} LARYNXP</span></a></div>
+            <div id="userdgov" class="mx-4 text-info" dmx-show="openordersapi.data.gov > 0"><a href="/me#spk"><span class="text-info">{{(openordersapi.data.gov/1000).formatNumber(3,'.',',')}} LARYNXG</span></a></div>
+            <div id="userhive" class="mx-4 text-danger"><a href="/me#hive"><span class="text-danger">{{accountapi.data.result[0].balance}}</span></a></div>
+            <div id="userhbd" class="mx-4 text-success"><a href="/me#hive"><span class="text-success">{{accountapi.data.result[0].hbd_balance}}</span></a></div>
+				  
           </div>
         </div>
 		  <!-- node collapse region -->
