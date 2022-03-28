c84a430eeff8a4b51d12563bb19bf5e28c8e5a2a
Steven Ettinger <sjettingerjr@gmail.com>
2022-03-28 16:26:55 -0300

    vue comments

--- App/Views/dex/larynx.php
+++ App/Views/dex/larynx.php
@@ -565,22 +565,22 @@ include_once( $path );
               <table role="table" aria-busy="false" aria-colcount="3" class="table table-dark bg-darker text-white-50 table-striped table-hover table-borderless mb-0" id="larynxnodes">
                 <thead role="rowgroup" class="">
                   <tr role="row" class="">
-                    <th role="columnheader" v-bind:class="{'col-sort':'filteraccount.usera == true || filteraccount.userd == true'}" class="" aria-colindex="1" > <div class="d-flex align-items-center">
+                    <th role="columnheader" v-bind:class="{'col-sort':filteraccount.usera}" v-bind:class="{'col-sort':filteraccount.userd}" class="" aria-colindex="1" > <div class="d-flex align-items-center">
                       <div class="mr-3">USER NAME</div>
-                      <button title="Sort Ascending" type="button" class="mx-1 btn btn-sm btn-dark" v-bind:class="{'bg-primary':'filteraccount.usera == true'}" @click="sort(filterusers.checked ? 'runners' : 'runnersSearch','account','asc');toggleAPI('usera')"> <i class="fas fa-caret-up"></i></button>
-                      <button title="Sort Descending" type="button" class="mx-1 btn btn-sm btn-dark" v-bind:class="{'bg-primary':'filteraccount.userd == true'}" @click="sort(filterusers.checked ? 'runners' : 'runnersSearch','account','desc');toggleAPI('userd')"> <i class="fas fa-caret-down"></i></button>
+                      <button title="Sort Ascending" type="button" class="mx-1 btn btn-sm btn-dark" v-bind:class="{'bg-primary':filteraccount}" @click="sort(filterusers.checked ? 'runners' : 'runnersSearch','account','asc');toggleAPI('usera')"> <i class="fas fa-caret-up"></i></button>
+                      <button title="Sort Descending" type="button" class="mx-1 btn btn-sm btn-dark" v-bind:class="{'bg-primary':filteraccount}" @click="sort(filterusers.checked ? 'runners' : 'runnersSearch','account','desc');toggleAPI('userd')"> <i class="fas fa-caret-down"></i></button>
                     </div>
                     </th>
-                    <th role="columnheader" v-bind:class="{'col-sort':'filteraccount.gova == true || filteraccount.govd == true'}" class="" aria-colindex="2" > <div class="d-flex align-items-center">
+                    <th role="columnheader" v-bind:class="{'col-sort':filteraccount.gova}" v-bind:class="{'col-sort':filteraccount.govd}" class="" aria-colindex="2" > <div class="d-flex align-items-center">
                       <div class="mr-3">GOV BAL</div>
                       <button title="Sort Ascending" type="button" class="mx-1 btn btn-sm btn-dark" v-bind:class="{'bg-primary':filteraccount.gova}" @click="sort(filterusers.checked ? 'runners' : 'runnersSearch','g','asc');toggleAPI('gova')" > <i class="fas fa-caret-up"></i></button>
                       <button title="Sort Descending" type="button" class="mx-1 btn btn-sm btn-dark" v-bind:class="{'bg-primary':filteraccount.govd}" @click="sort(filterusers.checked ? 'runners' : 'runnersSearch','g','desc');toggleAPI('govd')" > <i class="fas fa-caret-down"></i></button>
                     </div>
                     </th>
-                    <th role="columnheader" v-bind:class="{'col-sort':'filteraccount.apia == true || filteraccount.apid == true'}" class="" aria-colindex="3" > <div class="d-flex align-items-center">
+                    <th role="columnheader" v-bind:class="{'col-sort':filteraccount.apia}" v-bind:class="{'col-sort':filteraccount.apid}" class="" aria-colindex="3" > <div class="d-flex align-items-center">
                       <div class="mr-3">API (Click to load)</div>
-                      <button title="Sort Ascending" type="button" class="mx-1 btn btn-sm btn-dark" v-bind:class="{'bg-primary':'filteraccount.apia'}" @click="sort(filterusers.checked ? 'runners' : 'runnersSearch','api','asc');toggleAPI('apia')" > <i class="fas fa-caret-up"></i></button>
-                      <button title="Sort Descending" type="button" class="mx-1 btn btn-sm btn-dark" v-bind:class="{'bg-primary':'filteraccount.apid'}" @click="sort(filterusers.checked ? 'runners' : 'runnersSearch','api','desc');toggleAPI('apid')" > <i class="fas fa-caret-down"></i></button>
+                      <button title="Sort Ascending" type="button" class="mx-1 btn btn-sm btn-dark" v-bind:class="{'bg-primary':filteraccount.apia}" @click="sort(filterusers.checked ? 'runners' : 'runnersSearch','api','asc');toggleAPI('apia')" > <i class="fas fa-caret-up"></i></button>
+                      <button title="Sort Descending" type="button" class="mx-1 btn btn-sm btn-dark" v-bind:class="{'bg-primary':filteraccount.apid}" @click="sort(filterusers.checked ? 'runners' : 'runnersSearch','api','desc');toggleAPI('apid')" > <i class="fas fa-caret-down"></i></button>
                     </div>
                     </th>
                     <th role="columnheader" class="" aria-colindex="4"> 
