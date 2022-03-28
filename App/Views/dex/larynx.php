fac4dce8c22f4998f514759aa06dc1c5b53970d7
Steven Ettinger <sjettingerjr@gmail.com>
2022-03-28 13:53:50 -0300

    vue comments

--- App/Views/dex/larynx.php
+++ App/Views/dex/larynx.php
@@ -447,7 +447,9 @@ include_once( $path );
             </div>
             <div class="d-flex" title="{{behind}} Blocks Behind Hive">
               <button class="text-center btn btn-sm align-items-center ml-4" 
-						 v-bind:class="{'btn-outline-success':'behind < 30', 'btn-outline-warning':'behind >= 30 && behind < 60', 'btn-outline-danger':'behind >= 60'}"	
+						 :class="{'btn-outline-success':'behind < 30'}" 
+             :class="{'btn-outline-warning':'behind >= 30 && behind < 60'}"
+             :class="{'btn-outline-danger':'behind >= 60'}"	
 						type="button" data-toggle="collapse" data-target="#nodedrawer" aria-expanded="false" aria-controls="nodedrawer">
 				  <span class="small p-0 m-0"><i class="fas fa-circle mr-2"></i>{{ TOKEN }} | </span>
 				  <span class="small p-0 m-0" v-if="behind < 30">ONLINE</span>
