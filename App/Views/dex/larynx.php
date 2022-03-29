4c943e978f9e1f03a7cab54e5c969ca95c6166b5
Steven Ettinger <sjettingerjr@gmail.com>
2022-03-29 03:39:46 -0300

    vue

--- App/Views/dex/larynx.php
+++ App/Views/dex/larynx.php
@@ -1132,7 +1132,7 @@ include_once( $path );
                     <div role="group" class="input-group">
                       <input id="sellPrice" v-on:keyup="scalc('p')" v-model="sellPrice" type="number" placeholder="0" required step="0.000001" min="0.000001" aria-required="true" class="form-control bg-dark border-dark text-info"  :readonly="sform.pl">
                       <div class="input-group-append">
-                        <div class="input-group-text bg-dark border-dark text-white-50 r-radius-hotfix"><span v-if="buyhive.checked">HIVE</span><span v-if="buyhbd.checked">HBD</span>/{{TOKEN}}<a href="#/" class="ml-3 text-secondary" @click="block('p')" v-if="selllimit.checked"><i class="fas" :class="{'fa-lock':sform.pl, 'fa-unlock-alt':!sform.pl}"></i></a></div>
+                        <div class="input-group-text bg-dark border-dark text-white-50 r-radius-hotfix"><span v-if="buyhive.checked">HIVE</span><span v-if="buyhbd.checked">HBD</span>/{{TOKEN}}<a href="#/" class="ml-3 text-secondary" @click="slock('p')" v-if="selllimit.checked"><i class="fas" :class="{'fa-lock':sform.pl, 'fa-unlock-alt':!sform.pl}"></i></a></div>
                       </div>
                       <div class="invalid-feedback"> Minimum price is 0.000001 </div>
                     </div>
