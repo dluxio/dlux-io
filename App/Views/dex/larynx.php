9f8daa8c4c549cd6d5b694c9732b6f077d7326af
Steven Ettinger <sjettingerjr@gmail.com>
2022-03-29 14:32:27 -0300

    vue

--- App/Views/dex/larynx.php
+++ App/Views/dex/larynx.php
@@ -709,8 +709,8 @@ thead, tbody tr {
       },
       marketCap:{
         get() {
-          if(this.buyhive.checked)return `$${parseFloat((this.stats.tokenSupply/1000) * this.hiveprice.hive.usd * this.dexapi.markets.hive.tick).toFixed(2)}`
-          else return `$${parseFloat((this.stats.tokenSupply/1000) * this.hbdprice.hive_dollar.usd * this.dexapi.markets.hbd.tick).toFixed(2)}`
+          if(this.buyhive.checked)return `${parseFloat((this.stats.tokenSupply/1000) * this.hiveprice.hive.usd * this.dexapi.markets.hive.tick).toFixed(2)}`
+          else return `${parseFloat((this.stats.tokenSupply/1000) * this.hbdprice.hive_dollar.usd * this.dexapi.markets.hbd.tick).toFixed(2)}`
         }
       }
     }
