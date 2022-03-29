fe5fe0022bd94f0b186c417b59c6b1d2f7353f4f
Steven Ettinger <sjettingerjr@gmail.com>
2022-03-29 03:35:08 -0300

    vue

--- App/Views/dex/larynx.php
+++ App/Views/dex/larynx.php
@@ -662,12 +662,12 @@ thead, tbody tr {
     computed: {
       minbuy:{
         get() {
-          return parseFloat((this.buyPrice/1000).toFixed(3))+0.001
+          return parseFloat(parseFloat(this.buyPrice/1000).toFixed(3))+0.001
         }
       },
       minsell:{
         get() {
-          return parseFloat((this.sellPrice).toFixed(3))+0.001
+          return parseFloat(parseFloat(this.sellPrice).toFixed(3))+0.001
         }
       }
     }
