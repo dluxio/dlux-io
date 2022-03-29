de0ec0f72a31a212b4cf7f5928e78a2c2ade48f6
Steven Ettinger <sjettingerjr@gmail.com>
2022-03-29 02:03:49 -0300

    vue

--- App/Views/dex/larynx.php
+++ App/Views/dex/larynx.php
@@ -220,7 +220,7 @@ thead, tbody tr {
           case 't':
             if(this.sform.cl){
               if(this.sellhive.checked)this.sellPrice = (this.sellHiveTotal / this.sellQuantity).toFixed(6)
-              else this.buyPrice = (this.buyHBDTotal / this.buyQuantity).toFixed(6)
+              else this.sellPrice = (this.sellHBDTotal / this.sellQuantity).toFixed(6)
             } else {
               if(this.sellhive.checked)this.sellHiveTotal = (this.sellPrice * this.sellQuantity).toFixed(3)
               else this.sellHBDTotal = (this.sellPrice * this.sellQuantity).toFixed(3)
@@ -241,7 +241,7 @@ thead, tbody tr {
                 if(this.sellhive.checked)this.sellQuantity = (this.sellHiveTotal / this.sellPrice).toFixed(3)
                 else this.sellQuantity = (this.sellHBDTotal / this.sellPrice).toFixed(3)
               } else {
-                if(this.sellhive.checked)this.buyPrice = (this.sellHiveTotal / this.sellQuantity).toFixed(6)
+                if(this.sellhive.checked)this.sellPrice = (this.sellHiveTotal / this.sellQuantity).toFixed(6)
                 else this.sellPrice = (this.sellHBDTotal / this.sellQuantity).toFixed(6)
               }
             }
