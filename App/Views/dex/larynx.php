f2d78573d40cb541ae9a4270e088a2a2494ca991
Mark <m.e.giles@gmail.com>
2022-03-30 14:47:09 -0700

    Update larynx.php

--- App/Views/dex/larynx.php
+++ App/Views/dex/larynx.php
@@ -1034,7 +1034,9 @@
                 <!-- node status btn -->
                 <div :title="behindTitle" class="d-flex align-items-center mx-3">
                   <a class="text-center" style="font-size: .7em;" :class="{'text-success':'behind < 30'}" :class="{'text-warning':'behind >= 30 && behind < 60'}" :class="{'text-danger':'behind >= 60'}" type="button" data-toggle="collapse" data-target="#nodedrawer" aria-expanded="false" aria-controls="nodedrawer">
-                    <span class=" p-0 m-0"><i class="fas fa-circle mr-2"></i>{{ TOKEN }}</span>
+                    <span class=" p-0 m-0"><i class="fas fa-circle mr-2"></i><span class=" p-0 m-0" v-show="behind < 30">ONLINE</span>
+                        <span class=" p-0 m-0" v-show="behind >= 30 && behind <=100">LAGGING</span>
+                        <span class=" p-0 m-0" v-show="behind > 100">OFFLINE</span></span>
                   </a>
                 </div>
               </div>
