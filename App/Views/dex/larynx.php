567939200532d363d00c3bcdab8eb53c39772441
Steven Ettinger <sjettingerjr@gmail.com>
2022-03-30 17:44:25 -0300

    format

--- App/Views/dex/larynx.php
+++ App/Views/dex/larynx.php
@@ -363,7 +363,7 @@
           }, `${this.prefix}${this.features.powsel_up ? this.features.powup_id : this.features.powdn_id}`, `${this.features.powsel_up ? '' : 'Down-'}Powering ${this.TOKEN}...`, lapi.split('://')[1])
         },
         gov() {
-          broadcastCJA({
+          if (this.features.gov_val) broadcastCJA({
             amount: parseInt(this.features.gov_val * 1000)
           }, `${this.prefix}${this.features.govsel_up ? this.features.govup_id : this.features.govdn_id}`, `${this.features.govsel_up ? '' : 'Un-'}Locking ${this.TOKEN}...`, lapi.split('://')[1])
         },
@@ -1113,29 +1113,29 @@
                         <div class="form-group text-center">
                           <div class="btn-group btn-group-toggle my-2" data-toggle="buttons">
                             <label class="btn btn-outline-primary active">
-                              <input type="radio" name="pwrpair" id="pwrup" checked>
+                              <input type="radio" name="pwrpair" id="pwrup" :checked="features.powsel_up" @click="setValue('features.powsel_up', true)">
                               PWR UP </label>
                             <label class="btn btn-outline-primary">
-                              <input type="radio" name="pwrpair" id="pwrdown">
+                              <input type="radio" name="pwrpair" id="pwrdown" :checked="!features.powsel_up" @click="setValue('features.powsel_up', false)">
                               PWR DOWN </label>
                           </div>
                         </div>
                         <div class="form-group">
                           <label id="govuplarynxamountlab">Amount</label>
                           <div class="input-group">
-                            <input class="form-control bg-dark border-dark text-primary" v-if="pwrup.checked" required id="pwruplarynxamount" type="number" step="0.001" min="0.001" :max="parseFloat(bartoken)" placeholder="0.000">
-                            <input class="form-control bg-dark border-dark text-primary" v-if="pwrdown.checked" required id="pwrdownlarynxamount" type="number" step="0.001" min="0.001" :max="parseFloat(barpow)" placeholder="0.000">
+                            <input class="form-control bg-dark border-dark text-primary" v-if="features.powsel_up" v-model="features.pow_val" required id="pwruplarynxamount" type="number" step="0.001" min="0.001" :max="parseFloat(bartoken)" placeholder="0.000">
+                            <input class="form-control bg-dark border-dark text-primary" v-if="!features.powsel_up" v-model="features.pow_val" required id="pwrdownlarynxamount" type="number" step="0.001" min="0.001" :max="parseFloat(barpow)" placeholder="0.000">
                             <div class="input-group-append">
-                              <div class="input-group-text bg-dark border-dark text-white-50" v-if="pwrup.checked" id="govupformunits"> {{TOKEN}} </div>
-                              <div class="input-group-text bg-dark border-dark text-white-50" v-if="pwrdown.checked" id="govdownformunits"> {{TOKEN}}P </div>
+                              <div class="input-group-text bg-dark border-dark text-white-50" v-if="features.powsel_up" id="govupformunits"> {{TOKEN}} </div>
+                              <div class="input-group-text bg-dark border-dark text-white-50" v-if="!features.powsel_up" id="govdownformunits"> {{TOKEN}}P </div>
                             </div>
                           </div>
-                          <div class="small py-2" v-if="pwrup.checked"><a href="#/" @click="setValue('pwruplarynxamount',balance)" class="text-primary">{{formatNumber(bartoken,3,'.',',')}} {{TOKEN}}</a> Available</div>
-                          <div class="small pt-2" v-if="pwrdown.checked"><a href="#/" @click="setValue('pwrdownlarynxamount',balance)" class="text-primary">{{formatNumber(barpow,3,'.',',')}} {{TOKEN}}P</a> Available</div>
+                          <div class="small py-2" v-if="features.powsel_up"><a href="#/" @click="setValue('features.pow_val',balance)" class="text-primary">{{formatNumber(bartoken,3,'.',',')}} {{TOKEN}}</a> Available</div>
+                          <div class="small pt-2" v-if="!features.powsel_up"><a href="#/" @click="setValue('features.pow_val',accountapi.poweredUp/1000)" class="text-primary">{{formatNumber(barpow,3,'.',',')}} {{TOKEN}}P</a> Available</div>
                         </div>
                         <div class="text-center mt-3">
-                          <button id="pwruplarynxmodalsend" class="btn btn-primary" @click="gov()">Power Up<i class="fas fa-arrow-alt-circle-up ml-2"></i></button>
-                          <button id="pwrdownlarynxmodalsend" class="btn btn-primary" v-if="pwrdown.checked" @click="gov()">Power Down<i class="fas fa-arrow-alt-circle-down ml-2"></i></button>
+                          <button id="pwruplarynxmodalsend" class="btn btn-primary" v-if="features.powsel_up" @click="power()">Power Up<i class="fas fa-arrow-alt-circle-up ml-2"></i></button>
+                          <button id="pwrdownlarynxmodalsend" class="btn btn-primary" v-if="!features.powsel_up" @click="power()">Power Down<i class="fas fa-arrow-alt-circle-down ml-2"></i></button>
                         </div>
                       </form>
                     </div>
