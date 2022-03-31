9065350abe41502004f9df3b55fc8dd69e0f1837
Steven Ettinger <sjettingerjr@gmail.com>
2022-03-31 11:53:12 -0300

    fancy

--- App/Views/dex/index.php
+++ App/Views/dex/index.php
@@ -1698,7 +1698,7 @@
                   <div class=""></div>
                 </div>
               </div>
-              <trading-vue :data="ohlcv" />
+              <trading-vue :data="this.$data" />
             </div>
             <div class="mt-2 text-center d-flex justify-content-between">
               <div>
