82882fd8be9913a97b9508ad4817e0acfcf0c85e
Steven Ettinger <sjettingerjr@gmail.com>
2022-03-31 10:27:38 -0300

    fancy

--- App/Views/dex/index.php
+++ App/Views/dex/index.php
@@ -383,6 +383,9 @@
           },
         }
       },
+      compilerOptions: {
+        isCustomElement: tag => tag === 'TradingVue'
+      },
       components: {
         'TradingVue': TradingVue,
       },
