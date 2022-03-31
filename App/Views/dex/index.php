d8aaffb9c03a0c9f4bdb4e3fff3b1f0975545bcd
Steven Ettinger <sjettingerjr@gmail.com>
2022-03-31 00:30:13 -0300

    fancy

--- App/Views/dex/index.php
+++ App/Views/dex/index.php
@@ -125,7 +125,7 @@
     }
     if (lapi == 'https://token.dlux.io' || lapi == 'https://spkinstant.hivehoneycomb.com') {
       location.search = ''
-      location.pathname = location.pathname.replace('?', '')
+      //location.pathname = location.pathname.replace('?', '')
     }
     let user = localStorage.getItem('user') || 'GUEST'
     let hapi = localStorage.getItem('hapi') || 'https://api.hive.blog'
