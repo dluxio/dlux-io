4937dcb9320ffd42eda72f40132c68959deae440
Mark <m.e.giles@gmail.com>
2022-03-30 19:16:15 -0700

    Update index.php

--- App/Views/dex/index.php
+++ App/Views/dex/index.php
@@ -619,7 +619,7 @@
             this[key] = value
           }
         },
-        manualApi(url) {
+        setApi(url) {
           let api = url || prompt("Please enter your API", "https://spkinstant.hivehoneycomb.com");
           if (api != null) {
             localStorage.setItem('lapi', api)
@@ -1036,7 +1036,7 @@
                     <span class=" p-0 m-0"><i class="fas fa-circle mr-2"></i><span class=" p-0 m-0" v-show="behind < 30">ONLINE</span>
                       <span class=" p-0 m-0" v-show="behind >= 30 && behind <=100">LAGGING</span>
                       <span class=" p-0 m-0" v-show="behind > 100">OFFLINE</span></span>
-                    <span class="p-0 m-0 ml-1">({{runners.length}} <i class="fas fa-server"></i>)</span>
+                    <span class="p-0 m-0 ml-1">({{runners.length}} <i class="fas fa-user-lock"></i>)</span>
                   </a>
                 </div>
               </div>
@@ -1319,6 +1319,7 @@
                             </div>
                           </th>
                         </tr>
+						</thead>
                       <tbody role="rowgroup" class="tbody-scroll-nodes">
                         <tr class="" role="row" v-for="node in runners" v-if="!filteraccount.value">
                           <td role="cell" class="" aria-colindex="1"><a :href="atref(node.account)">@{{node.account}}</a></td>
