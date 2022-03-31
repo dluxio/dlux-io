0639b816aa377e7dfacc81b780b11602b8669b43
Steven Ettinger <sjettingerjr@gmail.com>
2022-03-31 02:34:44 -0300

    fancy

--- App/Views/dex/index.php
+++ App/Views/dex/index.php
@@ -95,7 +95,7 @@
     import {
       createApp
     } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
-    import TradingVue from '/js/trading-vue.min.js'
+    import { TradingVue } from '/js/trading-vue.min.js'
 
     let url = location.href.replace(/\/$/, "");
     let lapi = ''
