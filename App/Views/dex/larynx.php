1c2c4fdfd09b34c7adb6f2eae85aa6a649ca56b1
Steven Ettinger <sjettingerjr@gmail.com>
2022-03-30 22:45:18 -0300

    vue

--- App/Views/dex/larynx.php
+++ App/Views/dex/larynx.php
@@ -619,6 +619,13 @@
             this[key] = value
           }
         },
+        manualApi(url) {
+          let api = url || prompt("Please enter your API", "https://spkinstant.hivehoneycomb.com");
+          if (api != null) {
+            localStorage.setItem('lapi', api)
+            location.reload()
+          }
+        },
         suggestValue(key, value) {
           if (key.split('.').length > 1) {
             let keys = key.split('.')
@@ -983,18 +990,9 @@
                   <div class="dropdown show"> <a class="btn btn-sm btn-dark dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Token </a>
                     <div class="dropdown-menu bg-black rounded">
                       <h6 class="dropdown-header">HIVE / HBD</h6>
-                      <a class="dropdown-item text-white-50" href="/dex/dlux">DLUX</a> <a class="dropdown-item text-white-50" href="/dex/larynx">LARYNX</a>
+                      <a class="dropdown-item text-white-50" href="#/" @click="setApi('https://token.dlux.io')">DLUX</a> <a class="dropdown-item text-white-50" href="#/" @click="setApi('https://spkinstant.hivehoneycomb.com')">LARYNX</a>
                       <div class="dropdown-divider bg-light"></div>
-                      <a class="dropdown-item text-white-50" href="#/" onclick="manualApi()">Manual</a>
-                      <script>
-                        function manualApi() {
-                          let api = prompt("Please enter your API", "https://spkinstant.hivehoneycomb.com");
-                          if (api != null) {
-                            localStorage.setItem('lapi', api)
-                            location.reload()
-                          }
-                        }
-                      </script>
+                      <a class="dropdown-item text-white-50" href="#/" @click="setApi()">Manual</a>
                     </div>
                   </div>
                 </div>
