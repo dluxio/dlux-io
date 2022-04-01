e39e5802536b2b861e157798a28a0f17ff896d13
Steven Ettinger <sjettingerjr@gmail.com>
2022-04-01 19:34:43 -0300

    fancy

--- App/Views/dex/index.php
+++ App/Views/dex/index.php
@@ -103,9 +103,9 @@
     import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
     // import { VeeValidate } from 'https://cdn.jsdelivr.net/npm/vee-validate@latest/dist/vee-validate.js'
     // Vue.component('validation-provider', VeeValidate.ValidationProvider);
-    // const {
-    //   ValidationProvider
-    // } = VeeValidate
+    const {
+      ValidationProvider
+    } = VeeValidate
     console.log('VP', {
       VeeValidate,
       ValidationProvider
