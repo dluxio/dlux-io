980afda558296139c6ee2be9445c7154f56d1587
Mark <m.e.giles@gmail.com>
2022-04-01 19:17:48 -0700

    Update index.php

--- App/Views/dex/index.php
+++ App/Views/dex/index.php
@@ -1245,7 +1245,7 @@
                 <!-- token select -->
                 <div id="userselectapi">
                   <div class="dropdown show"> <a class="btn btn-sm btn-dark dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Token </a>
-                    <div class="dropdown-menu bg-darker rounded">
+                    <div class="dropdown-menu bg-black rounded">
                       <h6 class="dropdown-header">HIVE / HBD</h6>
                       <a class="dropdown-item text-white-50" href="#/" @click="setApi('https://token.dlux.io')">DLUX</a> <a class="dropdown-item text-white-50" href="#/" @click="setApi('https://spkinstant.hivehoneycomb.com')">LARYNX</a>
                       <div class="dropdown-divider bg-light d-none"></div>
