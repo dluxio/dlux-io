6d95735734297b9c0666c6ae658c1b830e986034
Steven Ettinger <sjettingerjr@gmail.com>
2022-04-07 00:41:42 -0300

    navue

---
+++ public/js/footvue.js
@@ -0,0 +1,44 @@
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  template: `
+  <footer class="footer mt-auto bg-dark pt-5">
+
+  <!-- Footer Elements -->
+  <div class="container text-center">
+
+        <div class="mb-5 ">
+		
+          <!-- Github -->
+          <a href="https://github.com/dluxio" target="_blank">
+            <i class="fab fa-github fa-lg mx-md-3 mx-2 fa-2x"></i>
+          </a>
+          <!-- Discord -->
+          <a href="https://discord.gg/Beeb38j" target="_blank">
+            <i class="fab fa-discord fa-lg mx-md-3 mx-2 fa-2x"></i>
+          </a>
+          <!-- Hive -->
+          <a href="https://peakd.com/@dlux-io" target="_blank">
+            <img src="/img/peakd_logo.svg" class="responsive pb-3" width="40px">
+          </a>
+          <!--Twitter -->
+          <a href="https://twitter.com/dluxxr" target="_blank">
+            <i class="fab fa-twitter fa-lg mx-md-3 mx-2 fa-2x"></i>
+          </a>
+        </div>
+
+
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright bg-darker text-center text-white py-3">© 2020 Copyright: 
+    <a href="https://www.dlux.io/about/" class="">dlux.io</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+  `}
