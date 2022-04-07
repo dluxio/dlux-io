2da69a92be22d0efc83ec3b11d8431a53fdea8bb
Steven Ettinger <sjettingerjr@gmail.com>
2022-04-07 02:26:33 -0300

    split

--- public/js/navue.js
+++ public/js/navue.js
@@ -4,13 +4,19 @@ export default {
       HAS: false,
       HKC: true,
       HSR: false,
+      user: ''
     }
   },
   emits: ['login', 'logout'],
-  props: ['user'],
   methods:{
     getUser(){
         this.user = localStorage.getItem('user');
+        this.login.emit(this.user);
+    },
+    logout(){
+        localStorage.removeItem('user');
+        this.user = ''
+        this.logout.emit();
     }
   },
   mounted() {
@@ -20,6 +26,11 @@ export default {
       avatar:{
           get(){
               return 'https://images.hive.blog/u/' + this.user + '/avatar'
+          }
+      },
+      loggedIn:{
+          get(){
+              return this.user !== 'GUEST' ? true : false;
           }
       }
   },
@@ -36,19 +47,19 @@ export default {
         <li class="nav-item"> <a class="nav-link" href="/dex#dlux">DEX</a></li>
         <li class="nav-item"> <a class="nav-link" href="/docs/">DOCS</a></li>
       </ul>
-	<ul class="navbar-nav mr-5" id="loginMenu" v-show="user == 'GUEST'">
+	<ul class="navbar-nav mr-5" id="loginMenu" v-show="!loggedIn">
 	<li class="nav-item"><a class="nav-link acct-link" href="/about/">About</a></li>
 	<li class="nav-item"><a class="nav-link acct-link" href="https://signup.hive.io/">Get Account</a></li>
 	<li class="nav-item"><input id="userLogin" placeholder="username" class="bg-darkg border-dark text-info"></li>
 	</ul>
-    <div class="mr-5" v-show="user" id="userMenu">
+    <div class="mr-5" v-show="loggedIn" id="userMenu">
 	  <ul class="nav navbar-nav">
 		<li class="nav-item my-auto">
 			<a class="nav-link" href="/new/" data-bs-toggle="tooltip"  title="Create a new app">
 				<i class="fas fa-fw fa-lg fa-plus me-2"></i></a></li>
 		<li class="nav-item dropdown">
 		  <a class="nav-link dropdown-toggle text-white-50" id="userDropdown" role="button" aria-expanded="false" data-bs-toggle="dropdown" href="#">
-			  <img src="" id="userImage" alt="" width="30" height="30" class="img-fluid rounded-circle bg-light mr-1 cover">
+			  <img :src="avatar" id="userImage" alt="" width="30" height="30" class="img-fluid rounded-circle bg-light mr-1 cover">
 			  <span id="userName">{{user}}</span></a>
           <ul class="dropdown-menu dropdown-menu-dark pt-0" aria-labelledby="userDropdown">
 			 <li><a class="dropdown-item" href="/me#blog/" onClick="showTab('blog')"><i class="fas fa-user fa-fw me-2"></i>Profile</a></li>
@@ -59,8 +70,8 @@ export default {
              <li><hr class="dropdown-divider"></li>
 			 <li><a class="dropdown-item" href="/about/"><i class="fas fa-info-circle fa-fw me-2"></i>About</a></li>
              <li><hr class="dropdown-divider"></li>
-             <li><a class="dropdown-item" href="#" onclick="logout()" data-toggle="modal" data-target="#loginModal"><i class="fas fa-user-friends me-2"></i>Switch User</a></li>
-			 <li><a class="dropdown-item" href="#" onclick="logout()"><i class="fas fa-power-off fa-fw me-2"></i>Logout</a></li>
+             <li><a class="dropdown-item" href="#" @click="logout()"><i class="fas fa-user-friends me-2"></i>Switch User</a></li>
+			 <li><a class="dropdown-item" href="#" @click="logout()"><i class="fas fa-power-off fa-fw me-2"></i>Logout</a></li>
 		</ul>
         </li>
       </ul>
